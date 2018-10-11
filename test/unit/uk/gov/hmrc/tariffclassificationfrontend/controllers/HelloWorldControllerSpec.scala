8de26463c948c9565f781a7bf4d647e901216b12
Stefano Sgorlon <stefano.sgorlon@digital.hmrc.gov.uk>
2018-10-11 12:01:38 +0100

    DIT-227: fixing build

--- test/unit/uk/gov/hmrc/tariffclassificationfrontend/controllers/HelloWorldControllerSpec.scala
+++ test/unit/uk/gov/hmrc/tariffclassificationfrontend/controllers/HelloWorldControllerSpec.scala
@@ -1,4 +1,20 @@
-package uk.gov.hmrc.tariffclassificationfrontend.controllers
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package unit.uk.gov.hmrc.tariffclassificationfrontend.controllers
 
 import org.scalatest.{Matchers, WordSpec}
 import org.scalatestplus.play.guice.GuiceOneAppPerSuite
@@ -8,6 +24,7 @@ import play.api.test.FakeRequest
 import play.api.test.Helpers._
 import play.api.{Configuration, Environment}
 import uk.gov.hmrc.tariffclassificationfrontend.config.AppConfig
+import uk.gov.hmrc.tariffclassificationfrontend.controllers.HelloWorld
 
 
 class HelloWorldControllerSpec extends WordSpec with Matchers with GuiceOneAppPerSuite {
@@ -22,6 +39,7 @@ class HelloWorldControllerSpec extends WordSpec with Matchers with GuiceOneAppPe
   val controller = new HelloWorld(messageApi, appConfig)
 
   "GET /" should {
+
     "return 200" in {
       val result = controller.helloWorld(fakeRequest)
       status(result) shouldBe Status.OK
@@ -34,4 +52,5 @@ class HelloWorldControllerSpec extends WordSpec with Matchers with GuiceOneAppPe
     }
 
   }
+
 }
