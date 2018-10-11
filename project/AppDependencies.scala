8438b03a50f3744be8d89ce282b88f7facbb5012
Stefano Sgorlon <gokyo@users.noreply.github.com>
2018-10-11 12:05:28 +0100

    DIT-227: fixing build (#6)
    * DIT-227: scalastyle and code coverage
    
    * DIT-227: cleaning
    
    * DIT-227: adding scalastyle plugin
    
    * DIT-227: fixing build

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,21 +1,17 @@
 import play.core.PlayVersion.current
-import play.sbt.PlayImport._
-import sbt.Keys.libraryDependencies
 import sbt._
 
 object AppDependencies {
 
   val compile = Seq(
-
     "uk.gov.hmrc"             %% "govuk-template"           % "5.23.0",
     "uk.gov.hmrc"             %% "play-ui"                  % "7.22.0",
     "uk.gov.hmrc"             %% "bootstrap-play-25"        % "3.9.0"
   )
 
   val test = Seq(
-    "org.scalatest"           %% "scalatest"                % "3.0.4"                 % "test",
-    "org.jsoup"               %  "jsoup"                    % "1.10.2"                % "test",
-    "com.typesafe.play"       %% "play-test"                % current                 % "test",
+    "org.scalatest"           %% "scalatest"                % "3.0.4"                 % "test, it",
+    "com.typesafe.play"       %% "play-test"                % current                 % "test, it",
     "org.pegdown"             %  "pegdown"                  % "1.6.0"                 % "test, it",
     "uk.gov.hmrc"             %% "service-integration-test" % "0.2.0"                 % "test, it",
     "org.scalatestplus.play"  %% "scalatestplus-play"       % "2.0.0"                 % "test, it"
