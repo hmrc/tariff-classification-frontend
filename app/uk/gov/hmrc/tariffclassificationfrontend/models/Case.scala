2a82913b0b11c0831e86801b7ed3692d2110432d
Ed Wilson <ed.wilson2@hotmail.co.uk>
2018-10-12 15:35:08 +0100

    Dit 227 Add cases view (#10)
    * DIT-227 Fix missing imports
    
    * DIT-227 Example Cases View

--- app/uk/gov/hmrc/tariffclassificationfrontend/models/Case.scala
+++ app/uk/gov/hmrc/tariffclassificationfrontend/models/Case.scala
@@ -1,4 +1,4 @@
-@*
+/*
  * Copyright 2018 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,18 +12,13 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *@
+ */
 
-@import uk.gov.hmrc.tariffclassificationfrontend.config.AppConfig
-@import uk.gov.hmrc.tariffclassificationfrontend.views.html.govuk_wrapper
-@(pageTitle: String, heading: String, message: String)(implicit request: Request[_], messages: Messages, appConfig: AppConfig)
+package uk.gov.hmrc.tariffclassificationfrontend.models
 
-@contentHeader = {
-  <h1>@heading</h1>
-}
-
-@mainContent = {
-  <p>@message</p>
-}
-
-@govuk_wrapper(appConfig = appConfig, title = pageTitle, contentHeader = Some(contentHeader), mainContent = mainContent)
+case class Case
+(
+  reference: String,
+  description: String,
+  traderName: String
+)
