23416da66451223e3a786fd329cd400245c55214
Ed Wilson <ed.wilson2@hotmail.co.uk>
2018-10-15 14:21:57 +0100

    DIT-227 Controller Test

---
+++ test/unit/uk/gov/hmrc/tariffclassificationfrontend/controllers/CasesControllerSpec.scala
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.tariffclassificationfrontend.controllers
+
+import org.scalatest.{Matchers, WordSpec}
+import org.scalatestplus.play.guice.GuiceOneAppPerSuite
+import play.api.http.Status
+import play.api.i18n.{DefaultLangs, DefaultMessagesApi}
+import play.api.test.FakeRequest
+import play.api.test.Helpers._
+import play.api.{Configuration, Environment}
+import uk.gov.hmrc.tariffclassificationfrontend.config.AppConfig
+
+
+class CasesControllerSpec extends WordSpec with Matchers with GuiceOneAppPerSuite {
+  val fakeRequest = FakeRequest("GET", "/")
+
+  val env = Environment.simple()
+  val configuration = Configuration.load(env)
+
+  val messageApi = new DefaultMessagesApi(env, configuration, new DefaultLangs(configuration))
+  val appConfig = new AppConfig(configuration, env)
+
+  val controller = new CasesController(messageApi, appConfig)
+
+  "GET /cases" should {
+
+    "return 200" in {
+      val result = controller.gateway(fakeRequest)
+      status(result) shouldBe Status.OK
+    }
+
+    "return HTML" in {
+      val result = controller.gateway(fakeRequest)
+      contentType(result) shouldBe Some("text/html")
+      charset(result) shouldBe Some("utf-8")
+    }
+
+  }
+
+}
