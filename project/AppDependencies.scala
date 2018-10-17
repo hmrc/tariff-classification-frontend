46b244e72b67c715505dbfdae04c9e45ba208664
Stefano Sgorlon <stefano.sgorlon@digital.hmrc.gov.uk>
2018-10-17 14:18:04 +0100

    BAU: cleaning

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -10,17 +10,19 @@ object AppDependencies {
     "uk.gov.hmrc"             %% "play-json-union-formatter"  % "1.4.0"
   )
 
+  val scope = "test, it"
+
   val test = Seq(
-    "com.github.tomakehurst"  %  "wiremock"                 % "2.19.0"    % "test, it",
-    "com.typesafe.play"       %% "play-test"                % current     % "test, it",
-    "org.assertj"             %  "assertj-core"             % "3.11.1"    % "test, it",
-    "org.mockito"             %  "mockito-core"             % "2.23.0"    % "test, it",
-    "org.pegdown"             %  "pegdown"                  % "1.6.0"     % "test, it",
-    "org.scalatest"           %% "scalatest"                % "3.0.4"     % "test, it",
-    "org.scalatestplus.play"  %% "scalatestplus-play"       % "2.0.1"     % "test, it",
-    "uk.gov.hmrc"             %% "hmrctest"                 % "3.2.0"     % "test, it",
-    "uk.gov.hmrc"             %% "http-verbs-test"          % "1.1.0"     % "test, it",
-    "uk.gov.hmrc"             %% "service-integration-test" % "0.3.0"     % "test, it"
+    "com.github.tomakehurst"  %  "wiremock"                 % "2.19.0"    % scope,
+    "com.typesafe.play"       %% "play-test"                % current     % scope,
+    "org.assertj"             %  "assertj-core"             % "3.11.1"    % scope,
+    "org.mockito"             %  "mockito-core"             % "2.23.0"    % scope,
+    "org.pegdown"             %  "pegdown"                  % "1.6.0"     % scope,
+    "org.scalatest"           %% "scalatest"                % "3.0.4"     % scope,
+    "org.scalatestplus.play"  %% "scalatestplus-play"       % "2.0.1"     % scope,
+    "uk.gov.hmrc"             %% "hmrctest"                 % "3.2.0"     % scope,
+    "uk.gov.hmrc"             %% "http-verbs-test"          % "1.1.0"     % scope,
+    "uk.gov.hmrc"             %% "service-integration-test" % "0.3.0"     % scope
   )
 
 }
