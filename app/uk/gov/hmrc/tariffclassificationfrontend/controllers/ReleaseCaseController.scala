2f3ffec03b2e7c8a3766403b71b2d310306e6f77
Ed Wilson <ed.wilson2@hotmail.co.uk>
2018-11-01 13:56:39 +0000

    DIT-246 Minor Resource Not Found refactor

--- app/uk/gov/hmrc/tariffclassificationfrontend/controllers/ReleaseCaseController.scala
+++ app/uk/gov/hmrc/tariffclassificationfrontend/controllers/ReleaseCaseController.scala
@@ -62,7 +62,7 @@ class ReleaseCaseController @Inject()(casesService: CasesService,
             case _ => Future.successful(Ok(views.html.case_not_found(reference)))
           }
         })
-        .getOrElse(Future.successful(Ok(views.html.resource_not_found(Some("Queue")))))
+        .getOrElse(Future.successful(Ok(views.html.resource_not_found("Queue"))))
     }
 
     releaseCaseForm.bindFromRequest.fold(onInvalidForm, onValidForm)
