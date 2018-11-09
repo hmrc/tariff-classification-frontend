07d7cdec7ab7002d20e7e44c8af5d9499c536590
Ed Wilson <ed.wilson2@hotmail.co.uk>
2018-11-09 11:01:54 +0000

    DIT-290 Use EnumJson from standard libary

--- app/uk/gov/hmrc/tariffclassificationfrontend/utils/JsonFormatters.scala
+++ app/uk/gov/hmrc/tariffclassificationfrontend/utils/JsonFormatters.scala
@@ -47,28 +47,8 @@ object JsonFormatters {
 
 object EnumJson {
 
-  def enumReads[E <: Enumeration](enum: E): Reads[E#Value] = new Reads[E#Value] {
-    def reads(json: JsValue): JsResult[E#Value] = json match {
-      case JsString(s) =>
-        try {
-          JsSuccess(enum.withName(s))
-        } catch {
-          case _: NoSuchElementException =>
-            throw new InvalidEnumException(enum.getClass.getSimpleName, s)
-        }
-      case _ => JsError("String value expected")
-    }
-  }
-
-  implicit def enumWrites[E <: Enumeration]: Writes[E#Value] = new Writes[E#Value] {
-    def writes(v: E#Value): JsValue = JsString(v.toString)
-  }
-
   implicit def format[E <: Enumeration](enum: E): Format[E#Value] = {
-    Format(enumReads(enum), enumWrites)
+    Format(Reads.enumNameReads(enum), Writes.enumNameWrites)
   }
 
 }
-
-class InvalidEnumException(className: String, input: String)
-  extends RuntimeException(s"Enumeration expected of type: '$className', but it does not contain '$input'")
