781b4262bf513a6aae3a16d1bcd2b9481258459a
Ed Wilson <ed.wilson2@hotmail.co.uk>
2018-12-14 14:48:16 +0000

    DIT-339 Test fixes

---
+++ test/unit/uk/gov/hmrc/tariffclassificationfrontend/service/EmailServiceTest.scala
@@ -0,0 +1,52 @@
+package uk.gov.hmrc.tariffclassificationfrontend.service
+
+import org.mockito.ArgumentMatchers.{any, refEq}
+import org.mockito.BDDMockito.given
+import org.mockito.Mockito.verify
+import org.scalatest.mockito.MockitoSugar
+import play.api.libs.json.Writes
+import uk.gov.hmrc.http.HeaderCarrier
+import uk.gov.hmrc.play.test.UnitSpec
+import uk.gov.hmrc.tariffclassificationfrontend.connector.EmailConnector
+import uk.gov.hmrc.tariffclassificationfrontend.models._
+
+import scala.concurrent.Future
+
+class EmailServiceTest extends UnitSpec with MockitoSugar {
+
+  private implicit val hc: HeaderCarrier = HeaderCarrier()
+  private val connector = mock[EmailConnector]
+  private val service = new EmailService(connector)
+
+  "Email Service 'sendCompleteCaseEmail'" should {
+    val aCase = mock[Case]
+    val application = mock[BTIApplication]
+    val contact = Contact("name", "email", None)
+
+    "Throw exception for non-bti" in {
+      given(aCase.application).willReturn(application)
+      given(application.isBTI).willReturn(false)
+
+      val exception = intercept[IllegalArgumentException] {
+        await(service.sendCaseCompleteEmail(aCase))
+      }
+      exception.getMessage shouldBe "Cannot send email for non BTI types"
+    }
+
+    "Delegate to connector" in {
+      given(aCase.reference).willReturn("ref")
+      given(aCase.application).willReturn(application)
+      given(application.isBTI).willReturn(true)
+      given(application.asBTI).willReturn(application)
+      given(application.contact).willReturn(contact)
+      given(application.goodName).willReturn("item")
+
+      given(connector.send(any[CaseCompletedEmail])(any[HeaderCarrier], any[Writes[Any]])).willReturn(Future.successful())
+
+      await(service.sendCaseCompleteEmail(aCase))
+
+      verify(connector).send(refEq(CaseCompletedEmail(Seq("email"), CaseCompletedEmailParameters("name", "ref", "item"))))(any[HeaderCarrier], any[Writes[Any]])
+    }
+  }
+
+}
