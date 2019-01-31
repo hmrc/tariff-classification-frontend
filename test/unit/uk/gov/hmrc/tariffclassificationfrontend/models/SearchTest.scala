ddc82de2739489a8a1bf87d49c961501a2925a7e
Ed Wilson <4864094+ed0906@users.noreply.github.com>
2019-01-31 09:46:37 +0000

    DIT-292 Test

---
+++ test/unit/uk/gov/hmrc/tariffclassificationfrontend/models/SearchTest.scala
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.tariffclassificationfrontend.models
+
+import uk.gov.hmrc.play.test.UnitSpec
+
+class SearchTest extends UnitSpec {
+
+  private val populatedSearch = Search(
+    traderName = Some("trader-name")
+  )
+
+  private val populatedParams: Map[String, Seq[String]] = Map(
+    "traderName" -> Seq("trader-name")
+  )
+
+  private val populatedQueryParam: String = "traderName=trader-name"
+
+  "Search Binder" should {
+    // When we add fields to Search these tests shouldn't need changing, only the fields above.
+
+    "Unbind Unpopulated Search to Query String" in {
+      Search.bindable.unbind("", Search()) shouldBe ""
+    }
+
+    "Unbind Populated Search to Query String" in {
+      Search.bindable.unbind("", populatedSearch) shouldBe populatedQueryParam
+    }
+
+    "Bind empty query string" in {
+      Search.bindable.bind("", Map()) shouldBe Some(Right(Search()))
+    }
+
+    "Bind populated query string" in {
+      Search.bindable.bind("", populatedParams) shouldBe Some(Right(populatedSearch))
+    }
+
+  }
+
+}
