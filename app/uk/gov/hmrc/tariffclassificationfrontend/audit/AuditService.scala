22d6df6f32dab87bf6981234ca366ba68db91cf6
Ed Wilson <4864094+ed0906@users.noreply.github.com>
2019-02-14 13:16:37 +0000

    DIT-668 Review Comments

--- app/uk/gov/hmrc/tariffclassificationfrontend/audit/AuditService.scala
+++ app/uk/gov/hmrc/tariffclassificationfrontend/audit/AuditService.scala
@@ -68,21 +68,6 @@ class AuditService @Inject()(auditConnector: DefaultAuditConnector) {
     )
   }
 
-  def auditCaseAppealChange(oldCase: Case, updatedCase: Case, operator: Operator)
-                           (implicit hc: HeaderCarrier): Unit = {
-    sendExplicitAuditEvent(
-      auditEventType = CaseAppealChange,
-      auditPayload = baseAuditPayload(updatedCase, operator) + (
-        "newAppealStatus" -> appealStatus(updatedCase),
-        "previousAppealStatus" -> appealStatus(oldCase)
-      )
-    )
-  }
-
-  private def appealStatus(c: Case): String = {
-    c.decision.flatMap(_.appeal).map(_.status.toString).getOrElse("None")
-  }
-
   def auditCaseSuppressed(oldCase: Case, updatedCase: Case, operator: Operator)
                          (implicit hc: HeaderCarrier): Unit = {
     sendExplicitAuditEvent(
@@ -99,13 +84,24 @@ class AuditService @Inject()(auditConnector: DefaultAuditConnector) {
     )
   }
 
+  def auditCaseAppealChange(oldCase: Case, updatedCase: Case, operator: Operator)
+                           (implicit hc: HeaderCarrier): Unit = {
+    sendExplicitAuditEvent(
+      auditEventType = CaseAppealChange,
+      auditPayload = baseAuditPayload(updatedCase, operator) + (
+        "newAppealStatus" -> appealStatus(updatedCase),
+        "previousAppealStatus" -> appealStatus(oldCase)
+      )
+    )
+  }
+
   def auditCaseReviewChange(oldCase: Case, updatedCase: Case, operator: Operator)
                            (implicit hc: HeaderCarrier): Unit = {
     sendExplicitAuditEvent(
       auditEventType = CaseReviewChange,
       auditPayload = baseAuditPayload(updatedCase, operator) + (
-        "newReviewStatus" -> updatedCase.decision.flatMap(_.review).map(_.status.toString).getOrElse("None"),
-        "previousReviewStatus" -> oldCase.decision.flatMap(_.review).map(_.status.toString).getOrElse("None")
+        "newReviewStatus" -> reviewStatus(updatedCase),
+        "previousReviewStatus" -> reviewStatus(oldCase)
       )
     )
   }
@@ -126,8 +122,15 @@ class AuditService @Inject()(auditConnector: DefaultAuditConnector) {
 
   private def sendExplicitAuditEvent(auditEventType: String, auditPayload: Map[String, String])
                                     (implicit hc: HeaderCarrier): Unit = {
-
     auditConnector.sendExplicitAudit(auditType = auditEventType, detail = auditPayload)
+  }
+
+  private def appealStatus(c: Case): String = {
+    c.decision.flatMap(_.appeal).map(_.status.toString).getOrElse("None")
+  }
+
+  private def reviewStatus(c: Case): String = {
+    c.decision.flatMap(_.review).map(_.status.toString).getOrElse("None")
   }
 
 }
