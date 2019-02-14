2dbaffd1cd4ef6123dd5570405b3ca0b64cf6336
pllaurado <1227542+pllaurado@users.noreply.github.com>
2019-02-14 08:54:29 +0000

    DIT-375 Reject tests

---
+++ test/unit/uk/gov/hmrc/tariffclassificationfrontend/service/CasesService_RejectCaseSpec.scala
@@ -0,0 +1,112 @@
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.tariffclassificationfrontend.service
+
+import org.mockito.ArgumentMatchers.{refEq, _}
+import org.mockito.BDDMockito._
+import org.mockito.Mockito.{never, reset, verify, verifyZeroInteractions}
+import org.scalatest.BeforeAndAfterEach
+import org.scalatest.mockito.MockitoSugar
+import uk.gov.hmrc.http.HeaderCarrier
+import uk.gov.hmrc.play.test.UnitSpec
+import uk.gov.hmrc.tariffclassificationfrontend.audit.AuditService
+import uk.gov.hmrc.tariffclassificationfrontend.config.AppConfig
+import uk.gov.hmrc.tariffclassificationfrontend.connector.BindingTariffClassificationConnector
+import uk.gov.hmrc.tariffclassificationfrontend.models._
+import uk.gov.hmrc.tariffclassificationfrontend.models.request.NewEventRequest
+import uk.gov.tariffclassificationfrontend.utils.Cases
+
+import scala.concurrent.Future.{failed, successful}
+
+class CasesService_RejectCaseSpec extends UnitSpec with MockitoSugar with BeforeAndAfterEach with ConnectorCaptor {
+
+  private implicit val hc: HeaderCarrier = HeaderCarrier()
+
+  private val manyCases = mock[Seq[Case]]
+  private val oneCase = mock[Option[Case]]
+  private val connector = mock[BindingTariffClassificationConnector]
+  private val emailService = mock[EmailService]
+  private val fileStoreService = mock[FileStoreService]
+  private val audit = mock[AuditService]
+  private val config = mock[AppConfig]
+  private val aCase = Cases.btiCaseExample
+
+  private val service = new CasesService(config, audit, emailService, fileStoreService, connector)
+
+  override protected def afterEach(): Unit = {
+    super.afterEach()
+    reset(connector, audit, oneCase, manyCases, config)
+  }
+
+  "Reject a Case" should {
+    "update case status to REJECTED" in {
+      // Given
+      val operator: Operator = Operator("operator-id", Some("Billy Bobbins"))
+      val originalCase = aCase.copy(status = CaseStatus.OPEN)
+      val caseUpdated = aCase.copy(status = CaseStatus.REJECTED)
+
+      given(connector.updateCase(any[Case])(any[HeaderCarrier])).willReturn(successful(caseUpdated))
+      given(connector.createEvent(refEq(caseUpdated), any[NewEventRequest])(any[HeaderCarrier])).willReturn(successful(mock[Event]))
+
+      // When Then
+      await(service.rejectCase(originalCase, operator)) shouldBe caseUpdated
+
+      verify(audit).auditCaseRejected(refEq(originalCase), refEq(caseUpdated), refEq(operator))(any[HeaderCarrier])
+
+      val caseUpdating = theCaseUpdating(connector)
+      caseUpdating.status shouldBe CaseStatus.REJECTED
+
+      val eventCreated = theEventCreatedFor(connector, caseUpdated)
+      eventCreated.operator shouldBe Operator("operator-id", Some("Billy Bobbins"))
+      eventCreated.details shouldBe CaseStatusChange(CaseStatus.OPEN, CaseStatus.REJECTED)
+    }
+
+    "not create event on update failure" in {
+      val operator: Operator = Operator("operator-id")
+      val originalCase = aCase.copy(status = CaseStatus.OPEN)
+
+      given(connector.updateCase(any[Case])(any[HeaderCarrier])).willReturn(failed(new RuntimeException()))
+
+      intercept[RuntimeException] {
+        await(service.rejectCase(originalCase, operator))
+      }
+
+      verifyZeroInteractions(audit)
+      verify(connector, never()).createEvent(refEq(aCase), any[NewEventRequest])(any[HeaderCarrier])
+    }
+
+    "succeed on event create failure" in {
+      // Given
+      val operator: Operator = Operator("operator-id")
+      val originalCase = aCase.copy(status = CaseStatus.OPEN)
+      val caseUpdated = aCase.copy(status = CaseStatus.REJECTED)
+
+      given(connector.updateCase(any[Case])(any[HeaderCarrier])).willReturn(successful(caseUpdated))
+      given(connector.createEvent(refEq(caseUpdated), any[NewEventRequest])(any[HeaderCarrier])).willReturn(failed(new RuntimeException()))
+
+      // When Then
+      await(service.rejectCase(originalCase, operator)) shouldBe caseUpdated
+
+      verify(audit).auditCaseRejected(refEq(originalCase), refEq(caseUpdated), refEq(operator))(any[HeaderCarrier])
+
+      val caseUpdating = theCaseUpdating(connector)
+      caseUpdating.status shouldBe CaseStatus.REJECTED
+
+    }
+  }
+
+}
