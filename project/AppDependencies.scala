0be7c566ecd08aefe6c63d89bfdeb727425ea403
Stefano Sgorlon <gokyo@users.noreply.github.com>
2019-02-15 11:30:59 +0000

    Update AppDependencies.scala

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,7 +4,7 @@ import sbt._
 object AppDependencies {
 
   val compile = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-play-25"          % "4.8.0",
+    "uk.gov.hmrc"             %% "bootstrap-play-25"          % "4.9.0",
     "uk.gov.hmrc"             %% "play-ui"                    % "7.32.0-play-25",
     "uk.gov.hmrc"             %% "play-json-union-formatter"  % "1.5.0"
   )
