9f18344a7789dfee7a91221e199b15b666238c9f
Steve Sugden <46934286+SteveSugden@users.noreply.github.com>
2019-02-15 12:10:35 +0000

    Initial UI changes for cancel reason

--- app/uk/gov/hmrc/tariffclassificationfrontend/controllers/CancelRulingController.scala
+++ app/uk/gov/hmrc/tariffclassificationfrontend/controllers/CancelRulingController.scala
@@ -20,8 +20,11 @@ import javax.inject.{Inject, Singleton}
 import play.api.i18n.MessagesApi
 import play.api.mvc._
 import uk.gov.hmrc.tariffclassificationfrontend.config.AppConfig
+import uk.gov.hmrc.tariffclassificationfrontend.forms.{CancelRulingForm, ReviewForm}
+import uk.gov.hmrc.tariffclassificationfrontend.models.CancelReason.CancelReason
 import uk.gov.hmrc.tariffclassificationfrontend.models.Case
 import uk.gov.hmrc.tariffclassificationfrontend.models.CaseStatus.COMPLETED
+import uk.gov.hmrc.tariffclassificationfrontend.models.ReviewStatus.ReviewStatus
 import uk.gov.hmrc.tariffclassificationfrontend.service.CasesService
 import uk.gov.hmrc.tariffclassificationfrontend.views
 
@@ -38,14 +41,40 @@ class CancelRulingController @Inject()(authenticatedAction: AuthenticatedAction,
   override protected val caseService: CasesService = casesService
 
   override protected def redirect: String => Call = routes.CaseController.applicationDetails
+
   override protected def isValidCase: Case => Boolean = _.status == COMPLETED
 
   def cancelRuling(reference: String): Action[AnyContent] = authenticatedAction.async { implicit request =>
-    getCaseAndRenderView(reference, c => successful(views.html.cancel_ruling(c)))
+    getCaseAndRenderView(
+      reference,
+      c => {
+        val existingReason = c.decision.flatMap(_.cancelReason)
+        val form = CancelRulingForm.form.fill(existingReason)
+        successful(views.html.cancel_ruling(c, form))
+      }
+    )
   }
 
   def confirmCancelRuling(reference: String): Action[AnyContent] = authenticatedAction.async { implicit request =>
-    getCaseAndRenderView(reference, casesService.cancelRuling(_, request.operator).map(views.html.confirm_cancel_ruling(_)))
+
+    CancelRulingForm.form.bindFromRequest().fold(
+      errors => {
+        getCaseAndRenderView(
+          reference,
+          c => successful(views.html.cancel_ruling(c, errors))
+        )
+      },
+      (reason: Option[CancelReason]) => {
+        getCaseAndRenderView(
+          reference,
+          c => {
+            // TODO - pass reason to service to persist
+            println(s"reason was $reason")
+            caseService.cancelRuling(c, request.operator).map(views.html.confirm_cancel_ruling(_))
+          }
+        )
+      }
+    )
   }
 
 }
