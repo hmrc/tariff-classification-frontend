bab9af7d23084a4c7d6635cd36e2eb3546a94e86
pllaurado <1227542+pllaurado@users.noreply.github.com>
2019-02-21 11:53:56 +0000

    DIT-750 format code

--- test/it/uk/gov/hmrc/tariffclassificationfrontend/CompleteCaseSpec.scala
+++ test/it/uk/gov/hmrc/tariffclassificationfrontend/CompleteCaseSpec.scala
@@ -54,7 +54,7 @@ class CompleteCaseSpec extends IntegrationTest with MockitoSugar {
 
       response.body should include("id=\"complete-case-button\"")
       response.body should include("disabled=\"disabled\"")
-      response.body should include(" Complete Case")
+      response.body should include("Complete Case")
 
     }
 
@@ -72,55 +72,55 @@ class CompleteCaseSpec extends IntegrationTest with MockitoSugar {
   }
 
   //TODO DIT-291 This returns 403s due to CSRF issues
-  //  "Case Complete Confirm" should {
-  //    val caseWithStatusOPEN = CasePayloads.jsonOf(Cases.btiCaseExample.copy(status = CaseStatus.OPEN))
-  //    val event = EventPayloads.event
-  //
-  //    val csrfProvider = app.injector.instanceOf[TokenProvider]
-  //
-  //    "return status 200" in {
-  //      // Given
-  //      givenAuthSuccess()
-  //      stubFor(get(urlEqualTo("/cases/1"))
-  //        .willReturn(aResponse()
-  //          .withStatus(OK)
-  //          .withBody(caseWithStatusOPEN))
-  //      )
-  //      stubFor(post(urlEqualTo("/cases/1/events"))
-  //        .willReturn(aResponse()
-  //          .withStatus(CREATED)
-  //          .withBody(event))
-  //      )
-  //      stubFor(post(urlEqualTo("/hmrc/email"))
-  //        .willReturn(aResponse()
-  //          .withStatus(ACCEPTED))
-  //      )
-  //      stubFor(post(urlEqualTo(s"/templates/${EmailType.COMPLETE}"))
-  //        .withRequestBody(new EqualToJsonPattern(fromResource("parameters_email-request.json"), true, false))
-  //        .willReturn(aResponse()
-  //          .withBody(fromResource("email_template-response.json"))
-  //          .withStatus(HttpStatus.SC_OK))
-  //      )
-  //
-  //      // When
-  //      val response: WSResponse = await(ws.url(s"http://localhost:$port/tariff-classification/cases/1/complete").post(Map("csrfToken" -> Seq(csrfProvider.generateToken))))
-  //
-  //      // Then
-  //      response.status shouldBe OK
-  //      response.body should include("<h3 class=\"heading-large mt-0\">Complete this case</h3>")
-  //    }
-  //
-  //    "redirect on auth failure" in {
-  //      // Given
-  //      givenAuthFailed()
-  //
-  //      // When
-  //      val response: WSResponse = await(ws.url(s"http://localhost:$port/tariff-classification/cases/1/complete").post(Map("csrfToken" -> Seq(csrfProvider.generateToken))))
-  //
-  //      // Then
-  //      response.status shouldBe OK
-  //      response.body should include("You are not authorised to access this page.")
-  //    }
-  //  }
+//  "Case Complete Confirm" should {
+//    val caseWithStatusOPEN = CasePayloads.jsonOf(Cases.btiCaseExample.copy(status = CaseStatus.OPEN))
+//    val event = EventPayloads.event
+//
+//    val csrfProvider = app.injector.instanceOf[TokenProvider]
+//
+//    "return status 200" in {
+//      // Given
+//      givenAuthSuccess()
+//      stubFor(get(urlEqualTo("/cases/1"))
+//        .willReturn(aResponse()
+//          .withStatus(OK)
+//          .withBody(caseWithStatusOPEN))
+//      )
+//      stubFor(post(urlEqualTo("/cases/1/events"))
+//        .willReturn(aResponse()
+//          .withStatus(CREATED)
+//          .withBody(event))
+//      )
+//      stubFor(post(urlEqualTo("/hmrc/email"))
+//        .willReturn(aResponse()
+//          .withStatus(ACCEPTED))
+//      )
+//      stubFor(post(urlEqualTo(s"/templates/${EmailType.COMPLETE}"))
+//        .withRequestBody(new EqualToJsonPattern(fromResource("parameters_email-request.json"), true, false))
+//        .willReturn(aResponse()
+//          .withBody(fromResource("email_template-response.json"))
+//          .withStatus(HttpStatus.SC_OK))
+//      )
+//
+//      // When
+//      val response: WSResponse = await(ws.url(s"http://localhost:$port/tariff-classification/cases/1/complete").post(Map("csrfToken" -> Seq(csrfProvider.generateToken))))
+//
+//      // Then
+//      response.status shouldBe OK
+//      response.body should include("<h3 class=\"heading-large mt-0\">Complete this case</h3>")
+//    }
+//
+//    "redirect on auth failure" in {
+//      // Given
+//      givenAuthFailed()
+//
+//      // When
+//      val response: WSResponse = await(ws.url(s"http://localhost:$port/tariff-classification/cases/1/complete").post(Map("csrfToken" -> Seq(csrfProvider.generateToken))))
+//
+//      // Then
+//      response.status shouldBe OK
+//      response.body should include("You are not authorised to access this page.")
+//    }
+//  }
 
 }
