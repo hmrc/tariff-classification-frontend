0502c2c73ff62f0ba4b113dede91d5d1c22005ad
Stefano Sgorlon <gokyo@users.noreply.github.com>
2019-03-05 21:28:49 +0000

    Update AppDependencies.scala

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -14,14 +14,14 @@ object AppDependencies {
   val test = Seq(
     "com.github.tomakehurst"  %  "wiremock"                 % "2.21.0"        % scope,
     "com.typesafe.play"       %% "play-test"                % current         % scope,
-    "org.mockito"             %  "mockito-core"             % "2.24.5"        % scope,
+    "org.mockito"             %  "mockito-core"             % "2.25.0"        % scope,
     "org.jsoup"               %  "jsoup"                    % "1.11.3"        % scope,
     "org.pegdown"             %  "pegdown"                  % "1.6.0"         % scope,
     "org.scalatest"           %% "scalatest"                % "3.0.4"         % scope,
     "org.scalatestplus.play"  %% "scalatestplus-play"       % "2.0.1"         % scope,
     "uk.gov.hmrc"             %% "hmrctest"                 % "3.6.0-play-25" % scope,
     "uk.gov.hmrc"             %% "http-verbs-test"          % "1.4.0-play-25" % scope,
-    "uk.gov.hmrc"             %% "service-integration-test" % "0.5.0-play-25" % scope
+    "uk.gov.hmrc"             %% "service-integration-test" % "0.6.0-play-25" % scope
   )
 
 }
