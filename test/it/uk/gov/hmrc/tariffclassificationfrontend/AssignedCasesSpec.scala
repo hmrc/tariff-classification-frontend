dbec6377a3ccfe5fa9a33636d724538e6adb2b88
Stefano Sgorlon <stefano.sgorlon@digital.hmrc.gov.uk>
2019-03-19 17:28:46 +0000

    BAU: skeleton for integration tests

---
+++ test/it/uk/gov/hmrc/tariffclassificationfrontend/AssignedCasesSpec.scala
@@ -0,0 +1,13 @@
+package uk.gov.hmrc.tariffclassificationfrontend
+
+import org.scalatest.mockito.MockitoSugar
+
+class AssignedCasesSpec extends IntegrationTest with MockitoSugar {
+
+  "Assigned Cases" should {
+
+    // TODO
+
+  }
+
+}
---
+++ test/it/uk/gov/hmrc/tariffclassificationfrontend/ReAssignCaseSpec.scala
@@ -0,0 +1,13 @@
+package uk.gov.hmrc.tariffclassificationfrontend
+
+import org.scalatest.mockito.MockitoSugar
+
+class ReAssignCaseSpec extends IntegrationTest with MockitoSugar {
+
+  "Re-Assign Case" should {
+
+    // TODO
+
+  }
+
+}
