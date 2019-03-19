3a6930675b0aeda2126a2479ee149c663ca73637
Nick Redshaw <15005227+retroredge@users.noreply.github.com>
2019-03-19 09:05:12 +0000

    DIT-873 Change the link on the error page

--- app/uk/gov/hmrc/tariffclassificationfrontend/controllers/SuppressCaseController.scala
+++ app/uk/gov/hmrc/tariffclassificationfrontend/controllers/SuppressCaseController.scala
@@ -37,7 +37,7 @@ class SuppressCaseController @Inject()(authenticatedAction: AuthenticatedAction,
                                        val messagesApi: MessagesApi,
                                        implicit val appConfig: AppConfig) extends RenderCaseAction {
 
-  private val form: Form[Boolean] = MandatoryBooleanForm.form("supress_case")
+  private val form: Form[Boolean] = MandatoryBooleanForm.form("suppress_case")
 
   override protected val config: AppConfig = appConfig
   override protected val caseService: CasesService = casesService
