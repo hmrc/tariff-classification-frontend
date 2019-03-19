9146ddb30d6e4196c876ee6c7bea852bf7d4be3c
pllaurado <1227542+pllaurado@users.noreply.github.com>
2019-03-19 15:31:45 +0000

    DIT-671 add test for print tab

---
+++ test/unit/uk/gov/hmrc/tariffclassificationfrontend/views/partials/PrintTabViewSpec.scala
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.tariffclassificationfrontend.views.partials
+
+import uk.gov.hmrc.tariffclassificationfrontend.views.ViewMatchers._
+import uk.gov.hmrc.tariffclassificationfrontend.views.ViewSpec
+import uk.gov.hmrc.tariffclassificationfrontend.views.html.partials.print_tab
+
+class PrintTabViewSpec extends ViewSpec {
+
+  "Print Tab" should {
+
+    "Render link with expected content'" in {
+
+      val doc = view(
+        print_tab(
+          linkText = "link-expected-text"
+        )
+      )
+
+      doc should containElementWithID("print-section")
+      assert(doc.getElementById("print-section").classNames.isEmpty)
+      doc should containElementWithID("print-link")
+      doc.getElementById("print-link") should containText("link-expected-text")
+    }
+
+    "Render link with classes content'" in {
+
+      val doc = view(
+        print_tab(
+          linkText = "link-expected-text",
+          linkClasses = Some("expected-class")
+        )
+      )
+
+      assert(doc.getElementById("print-section").hasClass("expected-class"))
+    }
+
+    "Render link with expected print tag name'" in {
+
+      val doc = view(
+        print_tab(
+          linkText = "link-expected-text",
+          printTag = "expected-print-tag"
+        )
+      )
+
+      doc.getElementById("print-link").attr("href").contains("expected-print-tag")
+    }
+
+  }
+
+}
