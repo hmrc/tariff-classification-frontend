db6d539277239487bf1149855da61c258925534d
Steve Sugden <46934286+SteveSugden@users.noreply.github.com>
2019-03-20 09:07:08 +0000

    BAU code tidy

--- test/it/uk/gov/hmrc/tariffclassificationfrontend/AssignedCasesSpec.scala
+++ test/it/uk/gov/hmrc/tariffclassificationfrontend/AssignedCasesSpec.scala
@@ -3,23 +3,15 @@ package uk.gov.hmrc.tariffclassificationfrontend
 import com.github.tomakehurst.wiremock.client.WireMock._
 import org.scalatest.mockito.MockitoSugar
 import play.api.libs.ws.WSResponse
-import play.api.test.Helpers.{CREATED, OK}
-import uk.gov.hmrc.tariffclassificationfrontend.models.{CaseStatus, Operator, Pagination}
-import uk.gov.tariffclassificationfrontend.utils.{CasePayloads, Cases, EventPayloads}
+import play.api.test.Helpers.OK
+import uk.gov.hmrc.tariffclassificationfrontend.models.Pagination
+import uk.gov.tariffclassificationfrontend.utils.CasePayloads
 
 class AssignedCasesSpec extends IntegrationTest with MockitoSugar {
-  val testCasesServiceUrl = "/cases?application_type=BTI&assignee_id=some&status=OPEN,REFERRED,SUSPENDED&sort_by=days-elapsed" +
+  private val testCasesServiceUrl = "/cases?application_type=BTI&assignee_id=some&status=OPEN,REFERRED,SUSPENDED&sort_by=days-elapsed" +
     s"&sort_direction=desc&page=1&page_size=${Pagination.unlimited}"
 
   "Assigned Cases" should {
-    val operator = Operator("1", Some("arthur"))
-    val caseWithStatusOPEN = CasePayloads.jsonOf(Cases.btiCaseExample
-      .copy(
-        status = CaseStatus.OPEN,
-        assignee = Some(operator)
-      )
-    )
-    val event = EventPayloads.event
 
     "return status 200 for get all assigned cases" in {
       // Given
