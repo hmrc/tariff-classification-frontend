dfcb1f24ddb1c5442bbc57ced5a01b7aca4ac429
Pol Llaurado Navarro <1227542+pllaurado@users.noreply.github.com>
2019-03-21 11:03:29 +0000

    Style changes - accessibility review (#155)
    * style-changes h1,h2,h3,h4  gov uk elements align
    
    * style-changes  remove grey background on input text box
    
    * style-changes  add hidden text for case status
    
    * style-changes  add hidden text and remove aria-labelledby
    
    * style-changes  empty table
    
    * style-changes  fix test
    
    * style-changes  fix test

--- test/unit/uk/gov/hmrc/tariffclassificationfrontend/views/QueueViewSpec.scala
+++ test/unit/uk/gov/hmrc/tariffclassificationfrontend/views/QueueViewSpec.scala
@@ -40,7 +40,7 @@ class QueueViewSpec extends ViewSpec {
       doc should containElementWithID("nav-menu-queue-queue1_name")
       doc should containElementWithID("nav-menu-queue-queue2_name")
 
-      doc should containElementWithID("cases_list-table")
+      doc shouldNot containElementWithID("cases_list-table")
 
       doc should containElementWithID("nav-menu-my-cases")
 
