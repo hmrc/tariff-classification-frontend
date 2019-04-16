3d7e7cd0a397975e54f316fdd8e8c1a258abfc6a
Ed Wilson <4864094+ed0906@users.noreply.github.com>
2019-04-16 15:55:52 +0100

    DIT-961 Tests

--- test/unit/uk/gov/hmrc/tariffclassificationfrontend/views/HistogramTest.scala
+++ test/unit/uk/gov/hmrc/tariffclassificationfrontend/views/HistogramTest.scala
@@ -21,6 +21,13 @@ import uk.gov.hmrc.tariffclassificationfrontend.models.ReportResult
 
 class HistogramTest extends UnitSpec {
 
+  "Histogram Bucket" should {
+    "count" in {
+      HistogramBucket(Seq(1,2,3)).count shouldBe 3
+      HistogramBucket(Seq(1,2)).count shouldBe 2
+    }
+  }
+
   "Histogram Bucket Interval" should {
     "calculate if a value is within its bounds" in {
       HistogramBucketInterval(None, None).contains(Integer.MIN_VALUE) shouldBe true
@@ -54,11 +61,113 @@ class HistogramTest extends UnitSpec {
 
       Histogram.calculate(data, buckets) shouldBe Histogram(
         Map[(Option[String], HistogramBucketInterval), HistogramBucket](
-          ((None, interval1), HistogramBucket(Seq(0,1))),
-          ((None, interval2), HistogramBucket(Seq(2,3))),
-          ((None, interval3), HistogramBucket(Seq(4,5)))
+          ((None, interval1), HistogramBucket(Seq(0, 1))),
+          ((None, interval2), HistogramBucket(Seq(2, 3))),
+          ((None, interval3), HistogramBucket(Seq(4, 5)))
         )
       )
+    }
+
+    "Build Groups from data with name" in {
+      val data = Seq(
+        ReportResult(group = Some("1"), value = Seq(0, 1, 2, 3, 4, 5))
+      )
+
+      Histogram.calculate(data, buckets) shouldBe Histogram(
+        Map[(Option[String], HistogramBucketInterval), HistogramBucket](
+          ((Some("1"), interval1), HistogramBucket(Seq(0, 1))),
+          ((Some("1"), interval2), HistogramBucket(Seq(2, 3))),
+          ((Some("1"), interval3), HistogramBucket(Seq(4, 5)))
+        )
+      )
+    }
+
+    "Build Groups from data" in {
+      val data = Seq(
+        ReportResult(group = Some("1"), value = Seq(0, 1, 2, 3, 4, 5)),
+        ReportResult(group = None, value = Seq(0, 1, 2, 3, 4, 5))
+      )
+
+      Histogram.calculate(data, buckets) shouldBe Histogram(
+        Map[(Option[String], HistogramBucketInterval), HistogramBucket](
+          ((None, interval1), HistogramBucket(Seq(0, 1))),
+          ((None, interval2), HistogramBucket(Seq(2, 3))),
+          ((None, interval3), HistogramBucket(Seq(4, 5))),
+          ((Some("1"), interval1), HistogramBucket(Seq(0, 1))),
+          ((Some("1"), interval2), HistogramBucket(Seq(2, 3))),
+          ((Some("1"), interval3), HistogramBucket(Seq(4, 5)))
+        )
+      )
+    }
+
+    "Get Bucket by Group & Interval" in {
+      val histogram = Histogram(
+        Map[(Option[String], HistogramBucketInterval), HistogramBucket](
+          ((None, interval1), HistogramBucket(Seq(0, 1))),
+          ((Some("1"), interval2), HistogramBucket(Seq(2, 3))),
+          ((Some("2"), interval3), HistogramBucket(Seq(4, 5)))
+        )
+      )
+
+      histogram.getBucket(None, interval1) shouldBe Some(HistogramBucket(Seq(0,1)))
+      histogram.getBucket(Some("1"), interval2) shouldBe Some(HistogramBucket(Seq(2, 3)))
+      histogram.getBucket(Some("2"), interval3) shouldBe Some(HistogramBucket(Seq(4, 5)))
+    }
+
+    "Get Buckets by Group" in {
+      val histogram = Histogram(
+        Map[(Option[String], HistogramBucketInterval), HistogramBucket](
+          ((None, interval1), HistogramBucket(Seq(0, 1))),
+          ((Some("1"), interval2), HistogramBucket(Seq(2, 3))),
+          ((Some("2"), interval3), HistogramBucket(Seq(4, 5)))
+        )
+      )
+
+      histogram.getBuckets(None) shouldBe Map(interval1 -> HistogramBucket(Seq(0,1)))
+      histogram.getBuckets(Some("1")) shouldBe Map(interval2 -> HistogramBucket(Seq(2, 3)))
+      histogram.getBuckets(Some("2")) shouldBe Map(interval3 -> HistogramBucket(Seq(4, 5)))
+    }
+
+    "Get Buckets by Interval" in {
+      val histogram = Histogram(
+        Map[(Option[String], HistogramBucketInterval), HistogramBucket](
+          ((None, interval1), HistogramBucket(Seq(0, 1))),
+          ((Some("1"), interval2), HistogramBucket(Seq(2, 3))),
+          ((Some("2"), interval3), HistogramBucket(Seq(4, 5)))
+        )
+      )
+
+      histogram.getBuckets(interval1) shouldBe Map(None -> HistogramBucket(Seq(0,1)))
+      histogram.getBuckets(interval2) shouldBe Map(Some("1") -> HistogramBucket(Seq(2, 3)))
+      histogram.getBuckets(interval3) shouldBe Map(Some("2") -> HistogramBucket(Seq(4, 5)))
+    }
+
+    "Filter by Interval" in {
+      val histogram = Histogram(
+        Map[(Option[String], HistogramBucketInterval), HistogramBucket](
+          ((None, interval1), HistogramBucket(Seq(0, 1))),
+          ((Some("1"), interval2), HistogramBucket(Seq(2, 3))),
+          ((Some("2"), interval3), HistogramBucket(Seq(4, 5)))
+        )
+      )
+
+      histogram.filterByInterval(_ == interval1) shouldBe Histogram(Map((None, interval1) -> HistogramBucket(Seq(0,1))))
+      histogram.filterByInterval(_ == interval2) shouldBe Histogram(Map((Some("1"), interval2) -> HistogramBucket(Seq(2, 3))))
+      histogram.filterByInterval(_ == interval3) shouldBe Histogram(Map((Some("2"), interval3) -> HistogramBucket(Seq(4, 5))))
+    }
+
+    "Filter by Group" in {
+      val histogram = Histogram(
+        Map[(Option[String], HistogramBucketInterval), HistogramBucket](
+          ((None, interval1), HistogramBucket(Seq(0, 1))),
+          ((Some("1"), interval2), HistogramBucket(Seq(2, 3))),
+          ((Some("2"), interval3), HistogramBucket(Seq(4, 5)))
+        )
+      )
+
+      histogram.filterByGroup(_.isEmpty) shouldBe Histogram(Map((None, interval1) -> HistogramBucket(Seq(0,1))))
+      histogram.filterByGroup(_.contains("1")) shouldBe Histogram(Map((Some("1"), interval2) -> HistogramBucket(Seq(2, 3))))
+      histogram.filterByGroup(_.contains("2")) shouldBe Histogram(Map((Some("2"), interval3) -> HistogramBucket(Seq(4, 5))))
     }
   }
 
