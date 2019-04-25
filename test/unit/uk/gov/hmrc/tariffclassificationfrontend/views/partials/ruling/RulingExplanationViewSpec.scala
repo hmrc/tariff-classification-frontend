30cb492d5cc31a9834a11eaff3030ad18fe69b1b
Nick Redshaw <15005227+retroredge@users.noreply.github.com>
2019-04-25 13:37:22 +0100

    DIT-975 Unit tests for view

---
+++ test/unit/uk/gov/hmrc/tariffclassificationfrontend/views/partials/ruling/RulingExplanationViewSpec.scala
@@ -0,0 +1,90 @@
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.tariffclassificationfrontend.views.partials.ruling
+
+import uk.gov.hmrc.tariffclassificationfrontend.models.CaseStatus
+import uk.gov.hmrc.tariffclassificationfrontend.views.ViewMatchers._
+import uk.gov.hmrc.tariffclassificationfrontend.views.ViewSpec
+import uk.gov.hmrc.tariffclassificationfrontend.views.html.partials.ruling.ruling_explanation
+import uk.gov.tariffclassificationfrontend.utils.Cases.{aCase, withDecision, withStatus, withoutDecision}
+
+class RulingExplanationViewSpec extends ViewSpec {
+
+  "Ruling explanation" should {
+
+    "hide the `Holder information` section when the case has no decision" in {
+      val c = aCase(
+        withStatus(CaseStatus.OPEN),
+        withoutDecision()
+      )
+
+      // When
+      val doc = view(ruling_explanation(c))
+
+      // Then
+      doc shouldNot containElementWithID("information-for-holder")
+    }
+
+    "hide the `Holder explanation` field value when the decision has a blank explanation" in {
+      val c = aCase(
+        withStatus(CaseStatus.OPEN),
+        withDecision(
+          explanation = Some("")
+        )
+      )
+
+      // When
+      val doc = view(ruling_explanation(c))
+
+      // Then
+      doc shouldNot containElementWithID("holder-explanation")
+    }
+
+    "hide the `Holder explanation` field value when the decision has no explanation" in {
+      val c = aCase(
+        withStatus(CaseStatus.OPEN)
+      )
+
+      // When
+      val doc = view(ruling_explanation(c))
+
+      // Then
+      doc shouldNot containElementWithID("holder-explanation")
+    }
+
+    "show the `Holder explanation` field value when the decision has an explanation" in {
+      val c = aCase(
+        withStatus(CaseStatus.OPEN),
+        withDecision(
+          explanation = Some("An explanation")
+        )
+      )
+
+      // When
+      val doc = view(ruling_explanation(c))
+
+      // Then
+      doc should containElementWithID("information-for-holder")
+      doc should containElementWithID("holder-explanation")
+    }
+
+
+
+
+
+  }
+}
