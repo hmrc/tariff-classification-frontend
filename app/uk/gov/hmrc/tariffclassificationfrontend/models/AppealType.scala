c62a83eacb58f1827c4f46b294830055322f66c2
Steve Sugden <46934286+SteveSugden@users.noreply.github.com>
2019-05-21 09:41:59 +0100

    DIT-1011 - code review comment

--- app/uk/gov/hmrc/tariffclassificationfrontend/models/AppealType.scala
+++ app/uk/gov/hmrc/tariffclassificationfrontend/models/AppealType.scala
@@ -33,5 +33,5 @@ object AppealType extends Enumeration {
   }
 
   type AppealType = Value
-  val REVIEW, APPEAL_TIER_1, APPEAL_TIER_2, COURT_OF_APPEALS, SUPREME_COURT, ADR = Value
+  val ADR, REVIEW, APPEAL_TIER_1, APPEAL_TIER_2, COURT_OF_APPEALS, SUPREME_COURT = Value
 }
