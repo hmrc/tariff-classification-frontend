6e51c88ca2050f6eaf06af47152d6fb383dbb819
Ed Wilson <4864094+ed0906@users.noreply.github.com>
2019-06-03 10:31:10 +0100

    DIT - Dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,8 +5,8 @@ object AppDependencies {
 
   val compile = Seq(
     "io.megl"                 %% "play-json-extra"            % "2.4.3",
-    "uk.gov.hmrc"             %% "bootstrap-play-25"          % "4.10.0",
-    "uk.gov.hmrc"             %% "play-ui"                    % "7.38.0-play-25",
+    "uk.gov.hmrc"             %% "bootstrap-play-25"          % "4.12.0",
+    "uk.gov.hmrc"             %% "play-ui"                    % "7.40.0-play-25",
     "uk.gov.hmrc"             %% "play-json-union-formatter"  % "1.5.0"
   )
 
@@ -20,8 +20,8 @@ object AppDependencies {
     "org.pegdown"             %  "pegdown"                  % "1.6.0"         % scope,
     "org.scalatest"           %% "scalatest"                % "3.0.4"         % scope,
     "org.scalatestplus.play"  %% "scalatestplus-play"       % "2.0.1"         % scope,
-    "uk.gov.hmrc"             %% "hmrctest"                 % "3.6.0-play-25" % scope,
-    "uk.gov.hmrc"             %% "http-verbs-test"          % "1.4.0-play-25" % scope,
+    "uk.gov.hmrc"             %% "hmrctest"                 % "3.8.0-play-25" % scope,
+    "uk.gov.hmrc"             %% "http-verbs-test"          % "1.6.0-play-25" % scope,
     "uk.gov.hmrc"             %% "service-integration-test" % "0.6.0-play-25" % scope
   )
 
