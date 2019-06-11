46a7fa2e7c50e65de5c9b4e424e3ea6e8e1b7a0a
Steve Sugden <46934286+SteveSugden@users.noreply.github.com>
2019-06-11 11:47:18 +0100

    DIT-1050 add permission checks to "action" links  (#242)
    * DIT-1050 add permission checks to "action" links as part of not-taking ownership / read-only journey
    
    * DIT-1050 add permission checks to "action" links as part of not-taking ownership / read-only journey
    
    * DIT-1050 - review comments & unit test

---
+++ app/uk/gov/hmrc/tariffclassificationfrontend/views/partials/ShowActions.scala
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.tariffclassificationfrontend.views.partials
+
+import uk.gov.hmrc.tariffclassificationfrontend.models.CaseStatus.CaseStatus
+import uk.gov.hmrc.tariffclassificationfrontend.models.{CaseStatus, Permission}
+import uk.gov.hmrc.tariffclassificationfrontend.models.request.AuthenticatedRequest
+
+class ShowActions(caseStatus: CaseStatus)(implicit request: AuthenticatedRequest[_]) {
+
+  def refer: Boolean = caseStatus == CaseStatus.OPEN && request.hasPermission(Permission.REFER_CASE)
+  def reject: Boolean = caseStatus == CaseStatus.OPEN && request.hasPermission(Permission.REJECT_CASE)
+  def suspend: Boolean = caseStatus == CaseStatus.OPEN && request.hasPermission(Permission.SUSPEND_CASE)
+  def release: Boolean = caseStatus == CaseStatus.NEW && request.hasPermission(Permission.RELEASE_CASE)
+  def suppress: Boolean = caseStatus == CaseStatus.NEW && request.hasPermission(Permission.SUPPRESS_CASE)
+  def reopen: Boolean = (caseStatus == CaseStatus.SUSPENDED || caseStatus == CaseStatus.REFERRED) && request.hasPermission(Permission.REOPEN_CASE)
+
+  def any: Boolean = refer || reject || suspend || release || suppress || reopen
+}
+
+object ShowActions {
+  def apply(caseStatus: CaseStatus)(implicit request: AuthenticatedRequest[_]): ShowActions = new ShowActions(caseStatus)
+}
--- test/unit/uk/gov/hmrc/tariffclassificationfrontend/views/partials/liabilities/LiabilityActionsViewSpec.scala
+++ test/unit/uk/gov/hmrc/tariffclassificationfrontend/views/partials/liabilities/LiabilityActionsViewSpec.scala
@@ -26,6 +26,34 @@ class LiabilityActionsViewSpec extends ViewSpec {
 
   "Liability Actions" should {
 
+    def containHeading = containElementWithTag("h3")
+    def containSuspend = containElementWithID("suspend-case-button")
+    def containRefer = containElementWithID("refer-case-button")
+    def containReject = containElementWithID("reject-case-button")
+    def containRelease = containElementWithID("release-case-button")
+    def containSuppress = containElementWithID("suppress_link")
+    def containReopen = containElementWithID("reopen-case-button")
+
+    "Render case with no permissions" in {
+      // Given
+      val c = aCase(
+        withStatus(CaseStatus.OPEN),
+        withLiabilityOrderApplication
+      )
+
+      // When
+      val doc = view(liability_actions(c, 0)(requestWithPermissions(), messages, appConfig))
+
+      // Then
+      doc shouldNot containHeading
+      doc shouldNot containSuspend
+      doc shouldNot containRefer
+      doc shouldNot containReject
+      doc shouldNot containRelease
+      doc shouldNot containSuppress
+      doc shouldNot containReopen
+    }
+
     "Render OPEN case with SUSPEND_CASE permission" in {
       // Given
       val c = aCase(
@@ -37,13 +65,35 @@ class LiabilityActionsViewSpec extends ViewSpec {
       val doc = view(liability_actions(c, 0)(requestWithPermissions(Permission.SUSPEND_CASE), messages, appConfig))
 
       // Then
-      doc should containElementWithID("refer-case-button")
-      doc should containElementWithID("reject-case-button")
-      doc should containElementWithID("suspend-case-button")
+      doc should containHeading
+      doc should containSuspend
 
-      doc should not (containElementWithID("release-case-button"))
-      doc should not (containElementWithID("suppress_link"))
-      doc should not (containElementWithID("reopen-case-button"))
+      doc shouldNot containRefer
+      doc shouldNot containReject
+      doc shouldNot containRelease
+      doc shouldNot containSuppress
+      doc shouldNot containReopen
+    }
+
+    "Render OPEN case with REJECT_CASE permission" in {
+      // Given
+      val c = aCase(
+        withStatus(CaseStatus.OPEN),
+        withLiabilityOrderApplication
+      )
+
+      // When
+      val doc = view(liability_actions(c, 0)(requestWithPermissions(Permission.REJECT_CASE), messages, appConfig))
+
+      // Then
+      doc should containHeading
+      doc should containReject
+
+      doc shouldNot containSuspend
+      doc shouldNot containRefer
+      doc shouldNot containRelease
+      doc shouldNot containSuppress
+      doc shouldNot containReopen
     }
 
     "Render OPEN case without SUSPEND_CASE permission" in {
@@ -57,7 +107,7 @@ class LiabilityActionsViewSpec extends ViewSpec {
       val doc = view(liability_actions(c, 0)(requestWithPermissions(), messages, appConfig))
 
       // Then
-      doc should not (containElementWithID("suspend-case-button"))
+      doc shouldNot containSuspend
     }
 
     "Render NEW case with RELEASE_CASE permission" in {
@@ -71,13 +121,14 @@ class LiabilityActionsViewSpec extends ViewSpec {
       val doc = view(liability_actions(c, 0)(requestWithPermissions(Permission.RELEASE_CASE), messages, appConfig))
 
       // Then
-      doc should containElementWithID("release-case-button")
+      doc should containHeading
+      doc should containRelease
 
-      doc should not (containElementWithID("refer-case-button"))
-      doc should not (containElementWithID("reject-case-button"))
-      doc should not (containElementWithID("suspend-case-button"))
-      doc should not (containElementWithID("suppress_link"))
-      doc should not (containElementWithID("reopen-case-button"))
+      doc shouldNot containRefer
+      doc shouldNot containReject
+      doc shouldNot containSuspend
+      doc shouldNot containSuppress
+      doc shouldNot containReopen
     }
 
     "Render NEW case with SUPPRESS_CASE permission" in {
@@ -91,16 +142,17 @@ class LiabilityActionsViewSpec extends ViewSpec {
       val doc = view(liability_actions(c, 0)(requestWithPermissions(Permission.SUPPRESS_CASE), messages, appConfig))
 
       // Then
-      doc should containElementWithID("suppress_link")
+      doc should containHeading
+      doc should containSuppress
 
-      doc should not (containElementWithID("refer-case-button"))
-      doc should not (containElementWithID("reject-case-button"))
-      doc should not (containElementWithID("suspend-case-button"))
-      doc should not (containElementWithID("release-case-button"))
-      doc should not (containElementWithID("reopen-case-button"))
+      doc shouldNot containRefer
+      doc shouldNot containReject
+      doc shouldNot containSuspend
+      doc shouldNot containRelease
+      doc shouldNot containReopen
     }
 
-    "Render SUSPENDED case" in {
+    "Render SUSPENDED case with REOPEN permission" in {
       // Given
       val c = aCase(
         withStatus(CaseStatus.SUSPENDED),
@@ -108,19 +160,20 @@ class LiabilityActionsViewSpec extends ViewSpec {
       )
 
       // When
-      val doc = view(liability_actions(c, 0)(requestWithPermissions(), messages, appConfig))
+      val doc = view(liability_actions(c, 0)(requestWithPermissions(Permission.REOPEN_CASE), messages, appConfig))
 
       // Then
-      doc should containElementWithID("reopen-case-button")
+      doc should containHeading
+      doc should containReopen
 
-      doc should not (containElementWithID("refer-case-button"))
-      doc should not (containElementWithID("reject-case-button"))
-      doc should not (containElementWithID("suspend-case-button"))
-      doc should not (containElementWithID("release-case-button"))
-      doc should not (containElementWithID("suppress_link"))
+      doc shouldNot containRefer
+      doc shouldNot containReject
+      doc shouldNot containSuspend
+      doc shouldNot containRelease
+      doc shouldNot containSuppress
     }
 
-    "Render REFERRED case" in {
+    "Render REFERRED case with REOPEN permission" in {
       // Given
       val c = aCase(
         withStatus(CaseStatus.REFERRED),
@@ -128,16 +181,17 @@ class LiabilityActionsViewSpec extends ViewSpec {
       )
 
       // When
-      val doc = view(liability_actions(c, 0)(requestWithPermissions(), messages, appConfig))
+      val doc = view(liability_actions(c, 0)(requestWithPermissions(Permission.REOPEN_CASE), messages, appConfig))
 
       // Then
-      doc should containElementWithID("reopen-case-button")
+      doc should containHeading
+      doc should containReopen
 
-      doc should not (containElementWithID("refer-case-button"))
-      doc should not (containElementWithID("reject-case-button"))
-      doc should not (containElementWithID("suspend-case-button"))
-      doc should not (containElementWithID("release-case-button"))
-      doc should not (containElementWithID("suppress_link"))
+      doc shouldNot containRefer
+      doc shouldNot containReject
+      doc shouldNot containSuspend
+      doc shouldNot containRelease
+      doc shouldNot containSuppress
     }
   }
 
