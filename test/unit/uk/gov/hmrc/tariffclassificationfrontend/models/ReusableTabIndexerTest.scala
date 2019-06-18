664cf3dc1c36cc9282b7f27d661ac9f22b94014a
aaroncarroll82 <aaron.carroll@digital.hmrc.gov.uk>
2019-06-18 12:09:21 +0100

    DIT-1048 - tab indexing and default selection for advanced search

---
+++ test/unit/uk/gov/hmrc/tariffclassificationfrontend/models/ReusableTabIndexerTest.scala
@@ -0,0 +1,41 @@
+package uk.gov.hmrc.tariffclassificationfrontend.models
+
+import uk.gov.hmrc.play.test.UnitSpec
+
+class ReusableTabIndexerTest extends UnitSpec {
+
+  "ReusableTabIndexer" should {
+
+    "start by default with expected first index" in {
+      val tabIndexer = ReusableTabIndexer()
+      tabIndexer.nextTabIndex() shouldBe 0
+    }
+
+    "increment by default with expected increment" in {
+      val tabIndexer = ReusableTabIndexer()
+      tabIndexer.nextTabIndex()
+      tabIndexer.nextTabIndex()
+      tabIndexer.nextTabIndex() shouldBe 2
+    }
+
+    "increment by jump with expected increment" in {
+      val tabIndexer = ReusableTabIndexer()
+      tabIndexer.nextTabIndex()
+      tabIndexer.nextTabIndexWithJump(50) shouldBe 50
+    }
+
+    "start at specified index when supplied" in {
+      val tabIndexer = ReusableTabIndexer(startTabIndex = 10)
+      tabIndexer.nextTabIndex() shouldBe 10
+    }
+
+    "increment by specified increment when supplied" in {
+      val tabIndexer = ReusableTabIndexer(indexIncrement = 100)
+      tabIndexer.nextTabIndex()
+      tabIndexer.nextTabIndex()
+      tabIndexer.nextTabIndex() shouldBe 200
+    }
+
+  }
+
+}
