842d7c17b20911e601edbe3ef73556ed5601c747
colesadam <colesadam@gmail.com>
2019-11-29 12:16:11 +0000

    DIT-1308 Focus now stays on search box when add to search button is pressed

--- app/uk/gov/hmrc/tariffclassificationfrontend/controllers/SearchController.scala
+++ app/uk/gov/hmrc/tariffclassificationfrontend/controllers/SearchController.scala
@@ -27,7 +27,7 @@ import uk.gov.hmrc.tariffclassificationfrontend.forms.SearchForm
 import uk.gov.hmrc.tariffclassificationfrontend.models._
 import uk.gov.hmrc.tariffclassificationfrontend.service.{CasesService, FileStoreService, KeywordsService}
 import uk.gov.hmrc.tariffclassificationfrontend.views.SearchTab.SearchTab
-import uk.gov.hmrc.tariffclassificationfrontend.views.html
+import uk.gov.hmrc.tariffclassificationfrontend.views.{SearchTab, html}
 import uk.gov.hmrc.tariffclassificationfrontend.views.partials.SearchResult
 
 import scala.concurrent.ExecutionContext.Implicits.global
@@ -42,9 +42,10 @@ class SearchController @Inject()(verify: RequestActions,
                                  val messagesApi: MessagesApi,
                                  implicit val appConfig: AppConfig) extends FrontendController with I18nSupport {
 
-  def search(selectedTab: SearchTab, reference: Option[String] = None, search: Search = Search(), sort: Sort = Sort(), page: Int):
+  def search(selectedTab: SearchTab,addToSearch: Option[Boolean] = None, reference: Option[String] = None, search: Search = Search(), sort: Sort = Sort(), page: Int):
     Action[AnyContent] = (verify.authenticated andThen verify.mustHave(Permission.ADVANCED_SEARCH)).async { implicit request =>
 
+    val focus: SearchTab = if(addToSearch.contains(true)) SearchTab.SEARCH_BOX else selectedTab
     if (reference.isDefined) {
       reference match {
         case Some(ref) if ref.trim.nonEmpty => successful(Redirect(routes.CaseController.get(ref.trim)))
@@ -52,21 +53,21 @@ class SearchController @Inject()(verify: RequestActions,
       }
     } else if (search.isEmpty) {
       keywordsService.autoCompleteKeywords.map { keywords: Seq[String] =>
-        Results.Ok(html.advanced_search(SearchForm.form, None, keywords, selectedTab))
+        Results.Ok(html.advanced_search(SearchForm.form, None, keywords, focus))
       }
     } else {
       keywordsService.autoCompleteKeywords.flatMap(keywords => {
         SearchForm.form.bindFromRequest.fold(
           formWithErrors => {
-            Future.successful(Results.Ok(html.advanced_search(formWithErrors, None, keywords, selectedTab)))
+            Future.successful(Results.Ok(html.advanced_search(formWithErrors, None, keywords, focus)))
           },
           data => for {
             cases: Paged[Case] <- casesService.search(search, sort, SearchPagination(page))
             attachments: Map[Case, Seq[StoredAttachment]] <- fileStoreService.getAttachments(cases.results)
             results: Paged[SearchResult] = cases.map(c => SearchResult(c, attachments.getOrElse(c, Seq.empty)))
-          } yield Results.Ok(html.advanced_search(SearchForm.form.fill(data), Some(results), keywords, selectedTab))
+          } yield Results.Ok(html.advanced_search(SearchForm.form.fill(data), Some(results), keywords, focus))
                             .addingToSession((backToSearchResultsLinkLabel, "search results"), (backToSearchResultsLinkUrl,
-                              s"${SearchController.search(selectedTab, None, search, sort, page).url}#advanced_search_keywords"))
+                              s"${SearchController.search(selectedTab,Some(false), None, search, sort, page).url}#advanced_search_keywords"))
         )
       })
     }
