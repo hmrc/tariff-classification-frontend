d8ac39a4336a6549aaa03900c3c433223d71c682
periklis <1190980+periklas@users.noreply.github.com>
2020-03-19 13:58:42 +0000

    DIT-1218 Tests for LiabilityViewModel

--- test/unit/models/viewmodels/LiabilityViewModelSpec.scala
+++ test/unit/models/viewmodels/LiabilityViewModelSpec.scala
@@ -1,8 +1,26 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package models.viewmodels
 
+import controllers.ActiveTab.Liability
 import models.CaseStatus
 import uk.gov.hmrc.play.test.UnitSpec
 import utils.Cases
+
 
 class LiabilityViewModelSpec extends UnitSpec {
 
@@ -12,10 +30,26 @@ class LiabilityViewModelSpec extends UnitSpec {
 
       val c = Cases.btiCaseExample.copy(status = CaseStatus.CANCELLED)
 
-      println(LiabilityViewModel.fromCase(c))
+      assert(LiabilityViewModel.fromCase(c) === LiabilityViewModel(CaseHeaderViewModel("Liability", "trader-business-name", "Laptop", "1", "CANCELLED", false)))
 
     }
 
+
+    "create a complete view model if it has an expired ruling" in {
+
+      val c = Cases.btiCaseWithExpiredRuling
+
+      assert(LiabilityViewModel.fromCase(c).caseHeaderViewModel.caseStatus === "EXPIRED")
+
+    }
+
+    "create a completed view model" in {
+
+      val c = Cases.btiCaseExample.copy(status = CaseStatus.COMPLETED)
+
+      assert(LiabilityViewModel.fromCase(c).caseHeaderViewModel.caseStatus === "COMPLETED")
+
+    }
   }
 
 }
