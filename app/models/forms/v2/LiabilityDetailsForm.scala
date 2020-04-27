1a90cc5bcb5eef18f133e46c0349486ee25ad553
frikiT <osipovvictor1994@gmail.com>
2020-04-27 15:02:28 +0100

    DIT-1813: finish edit c592 view page, and make page look like in prototype

--- app/models/forms/v2/LiabilityDetailsForm.scala
+++ app/models/forms/v2/LiabilityDetailsForm.scala
@@ -21,7 +21,7 @@ import java.time.Instant
 import models.forms.FormConstraints.dateMustBeInThePast
 import models.forms.FormDate
 import models.forms.mappings.FormMappings._
-import models.{Case, Contact, RepaymentClaim}
+import models.{Address, Case, Contact, RepaymentClaim, TraderContactDetails}
 import play.api.data.Form
 import play.api.data.Forms._
 
@@ -30,12 +30,23 @@ object LiabilityDetailsForm {
   private val emailRegex = """^[a-zA-Z0-9\.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$""".r
 
   def liabilityDetailsForm(existingLiability: Case): Form[Case] = Form[Case](
-    mapping[Case, Option[Instant], String, Option[String], Boolean,
-      Option[Instant], Option[String], Option[String], Option[String],
-      Option[String], String, String, Option[String], Option[String], Option[Instant]](
+    mapping[Case, Option[Instant], String, Option[String],Option[String], Option[String],
+      Option[String], Option[String], Option[String],Option[String], Option[String], Boolean,
+      Option[Instant], Option[String],Option[String], Option[String], Option[String],
+       String, String, Option[String], Option[String],
+      Option[Instant]](
       "entryDate" -> optional(FormDate.date("case.liability.error.entry-date")
         .verifying(dateMustBeInThePast("case.liability.error.entry-date.future"))),
       "traderName" -> textNonEmpty("case.liability.error.empty.trader-name"),
+      //TODO make sure dont need validation
+      "traderEmail" -> optional(text),
+      "traderPhone" -> optional(text),
+      "traderBuildingAndStreet" -> optional(text),
+      "traderTownOrCity" -> optional(text),
+      "traderCounty" -> optional(text),
+      "traderPostcode" -> optional(text),
+      "boardsFileNumber" -> optional(text),
+      //TODO ^^
       "btiReference" -> optional(text),
       "repaymentClaim" -> boolean,
       "dateOfReceipt" -> optional(FormDate.date("case.liability.error.entry-date")
@@ -56,13 +67,21 @@ object LiabilityDetailsForm {
 
   private def validEmailFormat(email: String): Boolean = email.trim.isEmpty || emailRegex.findFirstMatchIn(email.trim).nonEmpty
 
-  private def form2Liability(existingCase: Case): (Option[Instant], String, Option[String],
+  private def form2Liability(existingCase: Case): (
+    Option[Instant], String, Option[String], Option[String], Option[String], Option[String], Option[String],
+      Option[String],Option[String],Option[String],
     Boolean, Option[Instant], Option[String], Option[String], Option[String], Option[String], String, String, Option[String],
     Option[String], Option[Instant]) => Case = {
-    case (entryDate, traderName, btiReference, isRepaymentClaim, dateOfReceipt, goodName, entryNumber, traderCommodityCode, officerCommodityCode,
+    case (entryDate, traderName, traderEmail,
+    traderPhone, traderBuildingAndStreet, traderTownOrCity,
+    traderCounty, traderPostcode, boardsFileNumber, btiReference, isRepaymentClaim,
+    dateOfReceipt, goodName, entryNumber, traderCommodityCode, officerCommodityCode,
     contactName, contactEmail, contactPhone, dvrNumber, dateForRepayment) =>
-      existingCase.copy(application = existingCase.application.asLiabilityOrder.copy(
+      existingCase.copy(
+        caseBoardsFileNumber = boardsFileNumber,
+        application = existingCase.application.asLiabilityOrder.copy(
         traderName = traderName,
+        traderContactDetails = Some(TraderContactDetails(traderEmail, traderPhone, Some(Address(traderBuildingAndStreet.getOrElse(""),traderTownOrCity.getOrElse(""),traderCounty, traderPostcode)))),
         btiReference = btiReference,
         repaymentClaim = if (isRepaymentClaim) Some(RepaymentClaim(dvrNumber = dvrNumber, dateForRepayment = dateForRepayment)) else None,
         dateOfReceipt = dateOfReceipt,
@@ -75,14 +94,30 @@ object LiabilityDetailsForm {
       ))
   }
 
-  private def liability2Form(existingCase: Case): Option[(Option[Instant], String, Option[String],
+  private def liability2Form(existingCase: Case): Option[(
+    Option[Instant], String, Option[String],Option[String], Option[String],
+      Option[String], Option[String], Option[String],Option[String], Option[String],
     Boolean, Option[Instant], Option[String], Option[String],
-    Option[String], Option[String], String, String, Option[String], Option[String], Option[Instant])] = {
+    Option[String], Option[String], String, String, Option[String],
+      Option[String], Option[Instant]
+    )] = {
     val existingLiability = existingCase.application.asLiabilityOrder
+
+    def buildingAndStreet(): Option[String] = Some(existingLiability.traderContactDetails.fold("")(_.address.fold("")(_.buildingAndStreet)))
+    def townOrCity(): Option[String] = Some(existingLiability.traderContactDetails.fold("")(_.address.fold("")(_.townOrCity)))
+    def county(): Option[String] = existingLiability.traderContactDetails.flatMap(_.address.flatMap(_.county))
+    def postCode(): Option[String] = existingLiability.traderContactDetails.flatMap(_.address.flatMap(_.postCode))
 
     Some((
       existingLiability.entryDate,
       existingLiability.traderName,
+      existingLiability.traderContactDetails.flatMap(_.email),
+      existingLiability.traderContactDetails.flatMap(_.phone),
+      buildingAndStreet(),
+      townOrCity(),
+      county(),
+      postCode(),
+      existingCase.caseBoardsFileNumber,
       existingLiability.btiReference,
       existingLiability.repaymentClaim.isDefined,
       existingLiability.dateOfReceipt,
@@ -99,13 +134,23 @@ object LiabilityDetailsForm {
   }
 
   def liabilityDetailsCompleteForm(existingLiability: Case): Form[Case] = Form[Case](
-    mapping[Case, Option[Instant], String, Option[String], Boolean, Option[Instant],
+    mapping[Case, Option[Instant], String, Option[String], Option[String],
+      Option[String], Option[String], Option[String],Option[String], Option[String], Option[String], Boolean, Option[Instant],
       Option[String], Option[String], Option[String], Option[String], String,
       String, Option[String], Option[String], Option[Instant]](
       "entryDate" -> optional(FormDate.date("case.liability.error.entry-date")
         .verifying(dateMustBeInThePast("case.liability.error.entry-date.future")))
         .verifying("error.required", _.isDefined),
       "traderName" -> textNonEmpty("case.liability.error.empty.trader-name"),
+      //TODO find what need to validate
+      "traderEmail" -> optional(text),
+      "traderPhone" -> optional(text),
+      "traderBuildingAndStreet" -> optional(text),
+      "traderTownOrCity" -> optional(text),
+      "traderCounty" -> optional(text),
+      "traderPostcode" -> optional(text),
+      "boardsFileNumber" -> optional(text),
+      //TODO take a look ^^
       "btiReference" -> optional(nonEmptyText),
       "repaymentClaim" -> boolean,
       "dateOfReceipt" -> optional(FormDate.date("case.liability.error.entry-date")
