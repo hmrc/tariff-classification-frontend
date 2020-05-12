4f0289ff8f85fcada47c84348e3e7cd68fa67691
frikiT <osipovvictor1994@gmail.com>
2020-05-12 10:20:35 +0100

    DIT-2009: improve test case for FormConstraints, add some TODOs for future improvement

--- test/unit/models/forms/FormConstraintsTest.scala
+++ test/unit/models/forms/FormConstraintsTest.scala
@@ -23,6 +23,10 @@ import uk.gov.hmrc.play.test.UnitSpec
 
 class FormConstraintsTest extends UnitSpec {
 
+  private val currentYear: Int = Calendar.getInstance().get(Calendar.YEAR)
+  //TODO read from appconfig
+  private val yearLowBound: Int = 2010
+
   "FormConstraintsTest" when {
 
     "dateMustBeInThePast" should {
@@ -30,25 +34,34 @@ class FormConstraintsTest extends UnitSpec {
         val date = Calendar.getInstance()
         date.add(Calendar.DAY_OF_MONTH, 1)
         val dateInstant = date.toInstant
-        val result = FormConstraints.dateMustBeInThePast("date.must.be.in.past").apply(dateInstant)
+        val result = FormConstraints.dateMustBeInThePast("Test error").apply(dateInstant)
 
-        result shouldBe Invalid("date.must.be.in.past")
+        result shouldBe Invalid("Test error")
       }
 
       "return invalid when date entered is one month from now" in {
         val date = Calendar.getInstance()
         date.add(Calendar.MONTH, 1)
         val dateInstant = date.toInstant
-        val result = FormConstraints.dateMustBeInThePast("date.must.be.in.past").apply(dateInstant)
+        val result = FormConstraints.dateMustBeInThePast("Test error").apply(dateInstant)
 
-        result shouldBe Invalid("date.must.be.in.past")
+        result shouldBe Invalid("Test error")
       }
 
       "return valid when date entered is in the past (one month ago)" in {
         val date = Calendar.getInstance()
         date.add(Calendar.MONTH, -1)
         val dateInstant = date.toInstant
-        val result = FormConstraints.dateMustBeInThePast("date.must.be.in.past").apply(dateInstant)
+        val result = FormConstraints.dateMustBeInThePast("Test error").apply(dateInstant)
+
+        result shouldBe Valid
+      }
+
+      "return valid when date entered is in the past (1000 years ago)" in {
+        val date = Calendar.getInstance()
+        date.add(Calendar.YEAR, -1000)
+        val dateInstant = date.toInstant
+        val result = FormConstraints.dateMustBeInThePast("Test error").apply(dateInstant)
 
         result shouldBe Valid
       }
@@ -57,76 +70,94 @@ class FormConstraintsTest extends UnitSpec {
     //TODO Pass the correct error message
     "dateLowerBound" should {
 
-      "return valid when year entered is not before the defined year" in {
-        val definedLowerBoundsYear = 2010
+      "return valid when year entered is earliest valid" in {
         val date = Calendar.getInstance()
-        date.add(Calendar.YEAR, -10)
+        val yearsDiff = yearLowBound - currentYear
+        date.add(Calendar.YEAR, yearsDiff)
 
         val yearInstant = date.toInstant
-        val result = FormConstraints.dateLowerBound("Entry date error error error", definedLowerBoundsYear).apply(yearInstant)
+        val result = FormConstraints.dateLowerBound("Test error", yearLowBound).apply(yearInstant)
 
         result shouldBe Valid
       }
 
-      "return invalid when year entered is greater than the defined year" in {
-        val definedLowerBoundsYear = 2010
+      "return valid when year entered is in future" in {
         val date = Calendar.getInstance()
-        date.add(Calendar.YEAR, -20)
+        val yearsDiff = currentYear + 1
+        date.add(Calendar.YEAR, yearsDiff)
 
         val yearInstant = date.toInstant
-        val result = FormConstraints.dateLowerBound("Entry date error error error", definedLowerBoundsYear).apply(yearInstant)
+        val result = FormConstraints.dateLowerBound("Test error", yearLowBound).apply(yearInstant)
 
-        result shouldBe Invalid("Entry date error error error")
+        result shouldBe Valid
+      }
+
+      "return invalid when year entered is less than the defined year" in {
+        val date = Calendar.getInstance()
+        val yearsDiff = yearLowBound - currentYear - 1
+
+        date.add(Calendar.YEAR, yearsDiff)
+
+        val yearInstant = date.toInstant
+        val result = FormConstraints.dateLowerBound("Test error", yearLowBound).apply(yearInstant)
+
+        result shouldBe Invalid("Test error")
       }
     }
 
     "btiReferenceIsCorrectFormat" should {
       "return valid when btiReference entered is in the correct format" in {
-        val result = FormConstraints.btiReferenceIsCorrectFormat.apply("1234567")
+        val result = FormConstraints.btiReferenceIsCorrectFormat().apply("1234567")
 
         result shouldBe Valid
       }
 
       "return invalid when btiReference entered contains characters" in {
-        val result = FormConstraints.btiReferenceIsCorrectFormat.apply("ASDFASASF")
+        val result = FormConstraints.btiReferenceIsCorrectFormat().apply("A1234567890")
 
-        result shouldBe Invalid("case.v2.liability.c592.details_edit.bti_reference_error", """[0-9]{6,22}""")
+        result shouldBe Invalid("case.v2.liability.c592.details_edit.bti_reference_error", FormConstraints.btiRefRegex.pattern.pattern())
       }
 
       "return invalid when btiReference entered exceeds the maximum number (22)" in {
-        val result = FormConstraints.btiReferenceIsCorrectFormat.apply("123456789123456789012312")
+        val result = FormConstraints.btiReferenceIsCorrectFormat().apply("123456789123456789012312")
 
-        result shouldBe Invalid("case.v2.liability.c592.details_edit.bti_reference_error", """[0-9]{6,22}""")
+        result shouldBe Invalid("case.v2.liability.c592.details_edit.bti_reference_error", FormConstraints.btiRefRegex.pattern.pattern())
+      }
+
+      "return invalid when btiReference entered less than minimum number (5)" in {
+        val result = FormConstraints.btiReferenceIsCorrectFormat().apply("12345")
+
+        result shouldBe Invalid("case.v2.liability.c592.details_edit.bti_reference_error", FormConstraints.btiRefRegex.pattern.pattern())
       }
     }
 
     "entryNumberIsNumberOnly" should {
 
       "return valid when entry number does not contain characters" in {
-        val result = FormConstraints.entryNumberIsNumberOnly.apply("123456")
+        val result = FormConstraints.entryNumberIsNumberOnly().apply("123456")
 
         result shouldBe Valid
       }
 
       "return invalid when entry number contains characters" in {
-        val result = FormConstraints.entryNumberIsNumberOnly.apply("1assedf23456")
+        val result = FormConstraints.entryNumberIsNumberOnly().apply("1assedf23456")
 
-        result shouldBe Invalid("case.liability.error.entry-number", """^\d+$""")
+        result shouldBe Invalid("case.liability.error.entry-number", FormConstraints.numbersOnlyRegex.pattern.pattern())
       }
     }
 
     "dvrNumberIsNumberOnly" should {
 
       "return valid when dvr number does not contain characters" in {
-        val result = FormConstraints.dvrNumberIsNumberOnly.apply("12233456")
+        val result = FormConstraints.dvrNumberIsNumberOnly().apply("12233456")
 
         result shouldBe Valid
       }
 
       "return invalid when dvr number contains characters" in {
-        val result = FormConstraints.dvrNumberIsNumberOnly.apply("34fadf234")
+        val result = FormConstraints.dvrNumberIsNumberOnly().apply("34fadf234")
 
-        result shouldBe Invalid("case.liability.error.dvr-number", """^\d+$""")
+        result shouldBe Invalid("case.liability.error.dvr-number", FormConstraints.numbersOnlyRegex.pattern.pattern())
       }
     }
   }
