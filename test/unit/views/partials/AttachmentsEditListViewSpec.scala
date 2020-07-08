915fdcf7259e35a2ff4ea412388a076a5a8ea62b
shiromi <shiromi002@yahoo.co.uk>
2020-07-08 16:49:07 +0100

    DIT 1895 fixed issues on Select files to attach to the ruling (#360)
    * DIT-1895 initial work
    
    * DIT-1895 fixed issues on Select files to attach to the ruling
    
    * DIT-1895 removed showselect param
    
    * DIT-1895 removed if statement
    
    * DIT-1895 removed showSelect parameter from partial

---
+++ test/unit/views/partials/AttachmentsEditListViewSpec.scala
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package views.partials
+
+import java.time.{ZoneOffset, ZonedDateTime}
+
+import models.response.ScanStatus
+import models.{Operator, Permission}
+import utils.Cases
+import views.ViewMatchers._
+import views.ViewSpec
+import views.html.partials.attachments_edit_list
+
+class AttachmentsEditListViewSpec extends ViewSpec {
+
+  "Attachments Edit List" should {
+
+    "Render Nothing given no attachments" in {
+      // When
+      val doc = view(attachments_edit_list("MODULE", Seq.empty))
+
+      // Then
+      doc shouldNot containElementWithID("MODULE-div")
+      doc should containElementWithID("MODULE-empty-div")
+    }
+
+    "Render attachments" in {
+      val attachment = Cases.storedAttachment.copy(
+        id = "FILE_ID",
+        fileName = "name",
+        url = Some("url"),
+        scanStatus = Some(ScanStatus.READY),
+        timestamp = ZonedDateTime.of(2019,1,1,0,0,0,0,ZoneOffset.UTC).toInstant
+      )
+
+      // When
+      val doc = view(attachments_edit_list("MODULE", Seq(attachment)))
+
+      // Then
+      doc should containElementWithID("MODULE-div")
+      doc should containElementWithID("MODULE-row-0")
+      doc should containElementWithID("MODULE-row-0-title")
+      doc should containElementWithID("MODULE-row-0-date")
+      doc.getElementById("MODULE-row-0-title") should containText("name")
+      doc.getElementById("MODULE-row-0-date") should containText("01 Jan 2019")
+    }
+
+  }
+}
