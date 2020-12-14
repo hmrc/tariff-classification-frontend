ffd10d27dbb3145f40b7dbd0127cfb1923a3547f
oliwiaKot <48218839+oliwiaKot@users.noreply.github.com>
2020-12-14 14:32:56 +0000

    added the first screen of the create correspondence journey, added a controller and form

---
+++ app/controllers/CreateCorrespondenceController.scala
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import config.AppConfig
+import models.forms.CorrespondenceForm
+import javax.inject.{Inject, Singleton}
+import models.{CorrespondenceApplication, Permission}
+import play.api.data.Form
+import play.api.i18n.I18nSupport
+import play.api.mvc._
+import service.CasesService
+import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendController
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+
+@Singleton
+class CreateCorrespondenceController @Inject() (
+  verify: RequestActions,
+  casesService: CasesService,
+  mcc: MessagesControllerComponents,
+  implicit val appConfig: AppConfig
+) extends FrontendController(mcc)
+    with I18nSupport {
+
+  private val form: Form[CorrespondenceApplication] = CorrespondenceForm.newCorrespondenceForm
+
+  def get(): Action[AnyContent] = (verify.authenticated andThen verify.mustHave(Permission.CREATE_CASES)).async {
+    implicit request: Request[AnyContent] => Future.successful(Ok(views.html.v2.create_correspondence(form)))
+  }
+
+  def post(): Action[AnyContent] = (verify.authenticated andThen verify.mustHave(Permission.CREATE_CASES)).async {
+    implicit request =>
+      form.bindFromRequest.fold(
+        formWithErrors => Future.successful(Ok(views.html.v2.create_correspondence(formWithErrors))),
+        correspondenceApp =>
+          casesService.createCase(correspondenceApp, request.operator).map { caseCreated =>
+            Redirect(routes.CaseController.get(caseCreated.reference))
+          }
+      )
+
+  }
+}
---
+++ app/models/forms/v2/CorrespondenceForm.scala
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package models.forms
+import models.{CorrespondenceApplication, Address, Contact}
+import play.api.data.Form
+import play.api.data.Forms._
+import models.forms.mappings.FormMappings._
+
+object CorrespondenceForm {
+
+  private val emailRegex =
+    """^[a-zA-Z0-9\.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$""".r
+
+  private val form2Correspondence: (String, String, String) => CorrespondenceApplication = {
+    case (shortDescr, source, contactEmail) =>
+      CorrespondenceApplication(
+        correspondenceStarter = Some(source),
+        agentName = None,
+        contact = Contact("", contactEmail, None),
+        offline = false,
+        summary = shortDescr,
+        detailedDescription = "",
+        sampleToBeProvided = false,
+        sampleToBeReturned = false,
+        address = Address("", "", None, None)
+      )
+  }
+
+  private val correspondence2Form: CorrespondenceApplication => Option[(String, String, String)] = correspondence =>
+    Some((correspondence.summary, correspondence.correspondenceStarter.getOrElse(""), correspondence.contact.email))
+
+  val newCorrespondenceForm: Form[CorrespondenceApplication] = Form(
+    mapping(
+      "summary"   -> textNonEmpty("Please enter a short description"),
+      "source" -> textNonEmpty("Please enter a correspondence starter"),
+      "contactEmail" -> text.verifying("case.liability.error.email", e => validEmailFormat(e)),
+
+  )(form2Correspondence)(correspondence2Form)
+  )
+
+  private def validEmailFormat(email: String): Boolean =
+    email.trim.isEmpty || emailRegex.findFirstMatchIn(email.trim).nonEmpty
+}
