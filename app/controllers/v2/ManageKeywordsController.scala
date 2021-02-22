97f6d38ec10e79e93ccabc04a5b2be062843025c
Warren Smithson <36073378+warrenskiwork1979@users.noreply.github.com>
2021-02-22 11:53:41 +0000

    DIT-2755 | Added contoller createKeyword method. WIP

--- app/controllers/v2/ManageKeywordsController.scala
+++ app/controllers/v2/ManageKeywordsController.scala
@@ -18,25 +18,30 @@ package controllers.v2
 
 import com.google.inject.Inject
 import config.AppConfig
-import controllers.RequestActions
-import models.Permission
+import controllers.{RequestActions, routes}
+import models.{Case, Keyword, Permission}
 import models.forms.KeywordForm
 import models.viewmodels._
 import models.viewmodels.managementtools.ManageKeywordsViewModel
 import play.api.data.Form
 import play.api.i18n.I18nSupport
 import play.api.mvc._
+import service.ManageKeywordsService
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendController
 import views.html.managementtools.new_keyword_view
+
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
 
 class ManageKeywordsController @Inject()(
   verify: RequestActions,
   mcc: MessagesControllerComponents,
   val manageKeywordsView: views.html.managementtools.manage_keywords_view,
+  keywordService: ManageKeywordsService,
   implicit val appConfig: AppConfig
 ) extends FrontendController(mcc)
     with I18nSupport {
-  val keywordForm: Form[String] = KeywordForm.form
+  val keywordForm: Form[Keyword] = KeywordForm.form
 
   def displayManageKeywords(activeSubNav: SubNavigationTab = ManagerToolsKeywordsTab): Action[AnyContent] =
     (verify.authenticated andThen verify.mustHave(Permission.MANAGE_USERS))(implicit request =>
@@ -58,4 +63,29 @@ class ManageKeywordsController @Inject()(
         )
       )
     )
+
+  def createKeyword(): Action[AnyContent] = (verify.authenticated andThen verify.mustHave(Permission.MANAGE_USERS)).async {
+    implicit request =>
+      keywordForm.bindFromRequest.fold(
+        formWithErrors => Future.successful(Ok(new_keyword_view(
+          ManageKeywordsViewModel.forManagedTeams().allKeywordsTab, formWithErrors))),
+          keyword => keywordService.createKeyword(keyword).map {
+            saveKeyword: Keyword =>
+            Redirect(controllers.v2.routes.ManageKeywordsController.displayManageKeywords())
+          }
+      )
+  }
+
+
+//  def post(): Action[AnyContent] = (verify.authenticated andThen verify.mustHave(Permission.CREATE_CASES)).async {
+//    implicit request =>
+//      form.bindFromRequest.fold(
+//        formWithErrors => Future.successful(Ok(views.html.v2.create_correspondence(formWithErrors))),
+//        correspondenceApp =>
+//          casesService.createCase(correspondenceApp, request.operator).map { caseCreated: Case =>
+//            Redirect(routes.CreateCorrespondenceController.displayQuestion(caseCreated.reference))
+//          }
+//      )
+//
+//  }
 }
