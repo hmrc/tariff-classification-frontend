f1b65f0d73deda1783b8ef96351a9923c5838f8e
ryandac <ryan.da.costa@digital.hmrc.gov.uk>
2022-06-08 08:51:01 +0100

    DDCE-2586 - Fix error summary link for Radio Buttons and Checkboxes

--- build.sbt
+++ build.sbt
@@ -21,8 +21,8 @@ lazy val microservice = (project in file("."))
     scalaVersion := "2.12.12",
     targetJvm := "jvm-1.8",
     libraryDependencies ++= AppDependencies(),
-    parallelExecution in Test := false,
-    fork in Test := true,
+    Test / parallelExecution := false,
+    Test / fork := true,
     retrieveManaged := true,
     // Use the silencer plugin to suppress warnings from unused imports in compiled twirl templates
     scalacOptions += "-P:silencer:pathFilters=views;routes",
@@ -41,13 +41,13 @@ lazy val microservice = (project in file("."))
   )
   .settings(inConfig(TemplateTest)(Defaults.testSettings): _*)
   .settings(
-    unmanagedSourceDirectories in Test := Seq(
-      (baseDirectory in Test).value / "test/unit",
-      (baseDirectory in Test).value / "test/util"
+    Test / unmanagedSourceDirectories := Seq(
+      (Test / baseDirectory).value / "test/unit",
+      (Test / baseDirectory).value / "test/util"
     ),
-    resourceDirectory in Test := baseDirectory.value / "test" / "resources",
+    Test / resourceDirectory:= baseDirectory.value / "test" / "resources",
 //    works only when fork is true
-    javaOptions in Test += "-Xmx1G",
+    Test / javaOptions += "-Xmx1G",
     addTestReportOption(Test, "test-reports")
   )
   .settings(
@@ -72,16 +72,16 @@ lazy val microservice = (project in file("."))
   .settings(inConfig(IntegrationTest)(ScalafmtPlugin.scalafmtConfigSettings))
   .settings(inConfig(TemplateItTest)(Defaults.itSettings): _*)
   .settings(
-    Keys.fork in IntegrationTest := true,
+    IntegrationTest / Keys.fork := true,
 //    works only when fork is true
-    javaOptions in Test += "-Xmx1G",
-    unmanagedSourceDirectories in IntegrationTest := Seq(
-      (baseDirectory in IntegrationTest).value / "test/it",
-      (baseDirectory in IntegrationTest).value / "test/util"
+    Test / javaOptions += "-Xmx1G",
+    IntegrationTest / unmanagedSourceDirectories:= Seq(
+      (IntegrationTest / baseDirectory).value / "test/it",
+      (IntegrationTest / baseDirectory).value / "test/util"
     ),
-    resourceDirectory in IntegrationTest := baseDirectory.value / "test" / "resources",
+    IntegrationTest / resourceDirectory := baseDirectory.value / "test" / "resources",
     addTestReportOption(IntegrationTest, "int-test-reports"),
-    parallelExecution in IntegrationTest := false
+    IntegrationTest / parallelExecution := false
   )
 
   .settings(
@@ -98,6 +98,6 @@ lazy val TemplateTest   = config("tt") extend Test
 lazy val TemplateItTest = config("tit") extend IntegrationTest
 
 // Coverage configuration
-coverageMinimum := 91
+coverageMinimumStmtTotal := 91
 coverageFailOnMinimum := true
 coverageExcludedPackages := "<empty>;com.kenshoo.play.metrics.*;prod.*;testOnlyDoNotUseInAppConf.*;app.*;uk.gov.hmrc.BuildInfo;.*repositories.*"
--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,14 +2,14 @@ import sbt._
 object AppDependencies {
   import play.core.PlayVersion.current
 
-  val compile = Seq(
-    "uk.gov.hmrc"                %% "play-frontend-hmrc"            % "1.26.0-play-28",
-    "uk.gov.hmrc"                %% "bootstrap-frontend-play-28"    % "5.16.0",
-    "uk.gov.hmrc"                %% "http-caching-client"           % "9.5.0-play-28",
+  val compile: Seq[ModuleID] = Seq(
+    "uk.gov.hmrc"                %% "play-frontend-hmrc"            % "3.21.0-play-28",
+    "uk.gov.hmrc"                %% "bootstrap-frontend-play-28"    % "5.20.0",
+    "uk.gov.hmrc"                %% "http-caching-client"           % "9.6.0-play-28",
     "uk.gov.hmrc"                %% "simple-reactivemongo"          % "8.0.0-play-28",
     "com.typesafe.play"          %% "play-json"                     % "2.9.2",
     "uk.gov.hmrc"                %% "play-json-union-formatter"     % "1.15.0-play-28",
-    "org.typelevel"              %% "cats-core"                     % "2.6.1",
+    "org.typelevel"              %% "cats-core"                     % "2.7.0",
     "com.github.blemale"         %% "scaffeine"                     % "4.0.2",
     "com.lightbend.akka"         %% "akka-stream-alpakka-csv"       % "1.1.2"
   )
@@ -17,17 +17,17 @@ object AppDependencies {
   private lazy val scope: String = "test, it"
   val jettyVersion = "9.4.44.v20210927"
 
-  val test = Seq(
+  val test: Seq[ModuleID] = Seq(
     "com.github.tomakehurst" % "wiremock"                  % "2.27.2"         % scope,
     "com.typesafe.play"      %% "play-test"                % current          % scope,
-    "org.mockito"            % "mockito-core"              % "3.11.2"         % scope,
-    "org.jsoup"              % "jsoup"                     % "1.14.1"         % scope,
+    "org.mockito"            % "mockito-core"              % "4.6.1"         % scope,
+    "org.jsoup"              % "jsoup"                     % "1.15.1"         % scope,
     "org.pegdown"            % "pegdown"                   % "1.6.0"          % scope,
     "org.scalatest"          %% "scalatest"                % "3.2.9"          % scope,
     "org.scalatestplus"      %% "mockito-3-4"              % "3.2.9.0"        % scope,
     "org.scalatestplus"      %% "scalacheck-1-15"          % "3.2.9.0"        % scope,
-    "uk.gov.hmrc"            %% "bootstrap-test-play-28"   % "5.9.0"          % scope,
-    "uk.gov.hmrc"            %% "service-integration-test" % "1.2.0-play-28"  % scope,
+    "uk.gov.hmrc"            %% "bootstrap-test-play-28"   % "5.20.0"          % scope,
+    "uk.gov.hmrc"            %% "service-integration-test" % "1.3.0-play-28"  % scope,
     "com.vladsch.flexmark"    %  "flexmark-all"            % "0.36.8"        % scope,
 
     //Need to peg this version for wiremock - try removing this on next lib upgrade
