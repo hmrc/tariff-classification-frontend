380511da46311224d7b6512ba17cdddebe5b7576
nboaram <14924378+Nboaram@users.noreply.github.com>
2022-09-29 13:24:54 +0100

    [DDCE-2637] dep upgrades

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,12 +2,12 @@ import sbt._
 object AppDependencies {
   import play.core.PlayVersion.current
 
-  private val silencerVersion      = "1.7.9"
-  private val bootstrapPlayVersion = "7.2.0"
-  private val hmrcMongoPlayVersion = "0.71.0"
+  private val silencerVersion      = "1.7.11"
+  private val bootstrapPlayVersion = "7.4.0"
+  private val hmrcMongoPlayVersion = "0.73.0"
 
   val compile: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"                  %% "play-frontend-hmrc"         % "3.24.0-play-28",
+    "uk.gov.hmrc"                  %% "play-frontend-hmrc"         % "3.29.0-play-28",
     "uk.gov.hmrc"                  %% "bootstrap-frontend-play-28" % bootstrapPlayVersion,
     "uk.gov.hmrc.mongo"            %% "hmrc-mongo-play-28"         % hmrcMongoPlayVersion,
     "uk.gov.hmrc"                  %% "http-caching-client"        % "9.6.0-play-28",
@@ -27,9 +27,9 @@ object AppDependencies {
     "com.github.tomakehurst" % "wiremock-jre8"           % "2.33.2"             % scope,
     "com.typesafe.play"      %% "play-test"              % current              % scope,
     "org.jsoup"              % "jsoup"                   % "1.15.3"             % scope,
-    "org.scalatest"          %% "scalatest"              % "3.2.13"             % scope,
-    "org.scalatestplus"      %% "mockito-4-6"            % "3.2.13.0"           % scope,
-    "org.scalatestplus"      %% "scalacheck-1-16"        % "3.2.13.0"           % scope,
+    "org.scalatest"          %% "scalatest"              % "3.2.14"             % scope,
+    "org.scalatestplus"      %% "mockito-4-6"            % "3.2.14.0"           % scope,
+    "org.scalatestplus"      %% "scalacheck-1-16"        % "3.2.14.0"           % scope,
     "uk.gov.hmrc"            %% "bootstrap-test-play-28" % bootstrapPlayVersion % scope,
     "com.vladsch.flexmark"   % "flexmark-all"            % "0.62.2"             % scope
   )
