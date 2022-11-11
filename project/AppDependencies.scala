2a043b956b045eaa995229f051a5fdfa70205517
nboaram <14924378+Nboaram@users.noreply.github.com>
2022-11-11 11:17:07 +0000

    [DDCE-356] Dependency Upgrades

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,12 +2,12 @@ import sbt._
 object AppDependencies {
   import play.core.PlayVersion.current
 
-  private val silencerVersion      = "1.7.9"
-  private val bootstrapPlayVersion = "7.2.0"
-  private val hmrcMongoPlayVersion = "0.71.0"
+  private val silencerVersion      = "1.7.12"
+  private val bootstrapPlayVersion = "7.11.0"
+  private val hmrcMongoPlayVersion = "0.73.0"
 
   val compile: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"                  %% "play-frontend-hmrc"         % "3.24.0-play-28",
+    "uk.gov.hmrc"                  %% "play-frontend-hmrc"         % "3.32.0-play-28",
     "uk.gov.hmrc"                  %% "bootstrap-frontend-play-28" % bootstrapPlayVersion,
     "uk.gov.hmrc.mongo"            %% "hmrc-mongo-play-28"         % hmrcMongoPlayVersion,
     "uk.gov.hmrc"                  %% "http-caching-client"        % "9.6.0-play-28",
@@ -16,7 +16,7 @@ object AppDependencies {
     "org.typelevel"                %% "cats-core"                  % "2.8.0",
     "com.github.blemale"           %% "scaffeine"                  % "4.1.0",
     "com.lightbend.akka"           %% "akka-stream-alpakka-csv"    % "3.0.4",
-    "com.fasterxml.jackson.module" %% "jackson-module-scala"       % "2.13.4",
+    "com.fasterxml.jackson.module" %% "jackson-module-scala"       % "2.14.0",
     compilerPlugin("com.github.ghik" % "silencer-plugin" % silencerVersion cross CrossVersion.full),
     "com.github.ghik" % "silencer-lib" % silencerVersion % Provided cross CrossVersion.full
   )
@@ -27,9 +27,9 @@ object AppDependencies {
     "com.github.tomakehurst" % "wiremock-jre8"           % "2.33.2"             % scope,
     "com.typesafe.play"      %% "play-test"              % current              % scope,
     "org.jsoup"              % "jsoup"                   % "1.15.3"             % scope,
-    "org.scalatest"          %% "scalatest"              % "3.2.13"             % scope,
-    "org.scalatestplus"      %% "mockito-4-6"            % "3.2.13.0"           % scope,
-    "org.scalatestplus"      %% "scalacheck-1-16"        % "3.2.13.0"           % scope,
+    "org.scalatest"          %% "scalatest"              % "3.2.14"             % scope,
+    "org.scalatestplus"      %% "mockito-4-6"            % "3.2.14.0"           % scope,
+    "org.scalatestplus"      %% "scalacheck-1-16"        % "3.2.14.0"           % scope,
     "uk.gov.hmrc"            %% "bootstrap-test-play-28" % bootstrapPlayVersion % scope,
     "com.vladsch.flexmark"   % "flexmark-all"            % "0.62.2"             % scope
   )
