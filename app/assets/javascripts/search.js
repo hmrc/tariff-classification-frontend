14b10a7fab3b633601b3e06cd1a6d4cc8aad186a
mywyau <47248104+mywyau@users.noreply.github.com>
2023-05-03 16:00:57 +0100

    refactored search remove link js

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,8 +1,14 @@
 var advancedSearch = {
     removeKeyword: function(index) {
         var tbody = document.getElementById("advanced_search_keywords-list");
-        var row = document.getElementById("advanced_search_keywords-list-row-" + index);
-        tbody.removeChild(row);
-        document.getElementById("advanced_search-form").submit();
+        var row = document.getElementById(`advanced_search_keywords-list-row-${index}`);
+        var reply_click = function(){
+            tbody.removeChild(row);
+            document.getElementById("advanced_search-form").submit();
+        }
+        window.addEventListener("DOMContentLoaded", (event) => {
+            const element = document.getElementById(`advanced_search_keywords-list-row-${index}-remove_button`)
+              element.addEventListener('click', reply_click, false);
+        });
     }
 };
