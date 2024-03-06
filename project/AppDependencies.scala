0ad43d78650c1afede13e815c4eecac151f3e560
robpetterson <rob.petterson1@digital.hmrc.gov.uk>
2024-03-06 15:25:11 +0000

    DDCE-5034:Latest changes

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -9,14 +9,13 @@ object AppDependencies {
     "uk.gov.hmrc"                  %% "bootstrap-frontend-play-30" % bootstrapPlayVersion,
     "uk.gov.hmrc.mongo"            %% "hmrc-mongo-play-30"         % hmrcMongoPlayVersion,
     "com.typesafe.play"            %% "play-json"                  % "2.9.4",
-    "uk.gov.hmrc"                  %% "play-json-union-formatter-play-30"  % "1.18.0",
+    "uk.gov.hmrc"                  %% "play-json-union-formatter"  % "1.18.0-play-28",
     "org.typelevel"                %% "cats-core"                  % "2.10.0",
     "com.github.blemale"           %% "scaffeine"                  % "5.2.1",
     "com.lightbend.akka"           %% "akka-stream-alpakka-csv"    % "4.0.0", // avoid upgrading for now as akka 2.7.0 breaks bobby rules due to licensing
     "com.fasterxml.jackson.module" %% "jackson-module-scala"       % "2.15.3"
   )
 
-  private lazy val scope: String = "Test"
 
   val test: Seq[ModuleID] = Seq(
     "org.scalatest"        %% "scalatest"              % "3.2.17",
@@ -24,7 +23,7 @@ object AppDependencies {
     "org.scalatestplus"    %% "scalacheck-1-17"        % "3.2.17.0",
     "uk.gov.hmrc"          %% "bootstrap-test-play-30" % bootstrapPlayVersion,
     "com.vladsch.flexmark" % "flexmark-all"            % "0.64.8"
-  ).map(_ % scope)
+  ).map(_ % Test)
 
   def apply(): Seq[ModuleID] = compile ++ test
 
