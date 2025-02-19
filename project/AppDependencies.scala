792d87ac1adacd6765f2c3c2f141c9ab09175993
divya-manivannan-hmrc <175815017+Divya-Manivannan-hmrc@users.noreply.github.com>
2025-02-19 08:45:38 +0000

    DDCE-6420 Replace play-json-union-formatter Library

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,17 +3,16 @@ import sbt.*
 object AppDependencies {
 
   private val hmrcMongoPlayVersion = "2.4.0"
-  private val bootstrapPlayVersion = "9.6.0"
+  private val bootstrapPlayVersion = "9.9.0"
 
   private val compile: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"                  %% "play-frontend-hmrc-play-30" % "11.10.0",
+    "uk.gov.hmrc"                  %% "play-frontend-hmrc-play-30" % "11.11.0",
     "uk.gov.hmrc"                  %% "bootstrap-frontend-play-30" % bootstrapPlayVersion,
     "uk.gov.hmrc.mongo"            %% "hmrc-mongo-play-30"         % hmrcMongoPlayVersion,
-    "uk.gov.hmrc"                  %% "play-json-union-formatter"  % "1.22.0",
-    "org.typelevel"                %% "cats-core"                  % "2.12.0",
+    "org.typelevel"                %% "cats-core"                  % "2.13.0",
     "com.github.blemale"           %% "scaffeine"                  % "5.3.0",
     "com.fasterxml.jackson.module" %% "jackson-module-scala"       % "2.18.2",
-    "org.apache.pekko"             %% "pekko-connectors-csv"       % "1.0.2",
+    "org.apache.pekko"             %% "pekko-connectors-csv"       % "1.1.0",
     "org.apache.xmlgraphics"        % "fop"                        % "2.10",
     "net.sf.saxon"                  % "Saxon-HE"                   % "12.5"
   )
