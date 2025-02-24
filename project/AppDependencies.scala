14f4027f0bc8d2629f83a50207f1c08ded128ec7
divya-manivannan-hmrc <175815017+Divya-Manivannan-hmrc@users.noreply.github.com>
2025-02-24 10:28:24 +0000

    added UnionSpec and updated AppDependencies.scala

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,10 +3,10 @@ import sbt.*
 object AppDependencies {
 
   private val hmrcMongoPlayVersion = "2.5.0"
-  private val bootstrapPlayVersion = "9.6.0"
+  private val bootstrapPlayVersion = "9.9.0"
 
   private val compile: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"                  %% "play-frontend-hmrc-play-30" % "11.10.0",
+    "uk.gov.hmrc"                  %% "play-frontend-hmrc-play-30" % "11.11.0",
     "uk.gov.hmrc"                  %% "bootstrap-frontend-play-30" % bootstrapPlayVersion,
     "uk.gov.hmrc.mongo"            %% "hmrc-mongo-play-30"         % hmrcMongoPlayVersion,
     "org.typelevel"                %% "cats-core"                  % "2.13.0",
