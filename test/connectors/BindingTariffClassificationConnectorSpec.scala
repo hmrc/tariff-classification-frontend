e1d0cd3625face782f95513a9830d008e68a748d
Erick Borda <47341247+erickbm6922@users.noreply.github.com>
2025-06-23 16:21:01 +0100

    DDCE-6662 (#717)
    * Complete Implementation on removing the caseKeywords view on the frontend
    
    * Handle all ApplicationType scenarios instead of just null
    
    * Addressing PR bot comments

--- test/connectors/BindingTariffClassificationConnectorSpec.scala
+++ test/connectors/BindingTariffClassificationConnectorSpec.scala
@@ -1212,7 +1212,13 @@ class BindingTariffClassificationConnectorSpec extends ConnectorTest with CaseQu
       val caseHeader  = CaseHeader("ref", None, None, None, ApplicationType.ATAR, CaseStatus.REFERRED, 0, None)
       val caseKeyword = CaseKeyword(keyword, List(caseHeader))
 
-      val response = Json.toJson(Paged(Seq(caseKeyword))).toString()
+      // Create the ManageKeywordsData structure that the endpoint now returns
+      val manageKeywordsData = ManageKeywordsData(
+        pagedCaseKeywords = Paged(Seq(caseKeyword)),
+        pagedKeywords = Paged(Seq(keyword)) // Include the keywords data
+      )
+
+      val response = Json.toJson(manageKeywordsData).toString()
 
       stubFor(
         get(urlEqualTo("/case-keywords"))
@@ -1223,7 +1229,10 @@ class BindingTariffClassificationConnectorSpec extends ConnectorTest with CaseQu
           )
       )
 
-      await(connector.getCaseKeywords()) shouldBe Paged(Seq(caseKeyword))
+      // Update the assertion to match the new return type
+      val result = await(connector.getCaseKeywords())
+      result.pagedCaseKeywords shouldBe Paged(Seq(caseKeyword))
+      result.pagedKeywords     shouldBe Paged(Seq(keyword))
 
       verify(
         getRequestedFor(urlEqualTo("/case-keywords"))
